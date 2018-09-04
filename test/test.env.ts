8d3b6cea462d876c8a02d039e643bd5ee54826af
Dan Labrecque <dlabrecq@redhat.com>
2018-09-04 17:00:55 -0400

    Update patternfly-react packages to use latest PF4 typings and fixes
    Fixed cost details, vertical nav, and text input to use new PF4 styles
    Added aria-label to masthead's plain button to fix linter error
    Removed temporary Modal typings as a PF4 workaround
    Removed preventInjection function no longer exported by PF4 to fix tests
    Regenerated snapshots to fix tests
    
    Fixes: https://github.com/project-koku/koku-ui/issues/102

--- test/test.env.ts
+++ test/test.env.ts
@@ -1,8 +1,6 @@
-import { preventInjection } from '@patternfly/react-styles';
 import { StyleSheetTestUtils } from 'aphrodite';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 StyleSheetTestUtils.suppressStyleInjection();
-preventInjection();
 Enzyme.configure({ adapter: new Adapter() });
