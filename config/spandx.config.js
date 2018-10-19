3045102067f0eeabf9a844e5f7d2714d744d3203
Chris Hambridge <chambrid@redhat.com>
2018-10-19 06:32:04 -0400

    Remove all the work arounds now that both the JWT token and the Akamai rewrite rule is in place.

--- config/spandx.config.js
+++
@@ -1,8 +0,0 @@
-/* global exports */
-const insightsGateway = `https://api.access.redhat.com`;
-
-exports.routes = {
-  '/r/insights': {
-    host: insightsGateway,
-  },
-};
--- src/api/api.ts
+++ src/api/api.ts
@@ -7,18 +7,8 @@ export interface PagedResponse<D = any> {
   results: D[];
 }
 
-export function initApi({
-  host,
-  version,
-  token,
-}: {
-  host: string;
-  version: string;
-  token: string;
-}) {
+export function initApi({ version }: { version: string }) {
   axios.defaults.baseURL = `/r/insights/platform/cost-management/api/${version}/`;
-  axios.defaults.baseURL = `${host}/r/insights/platform/cost-management/api/${version}/`;
-  axios.defaults.headers.common.Authorization = `Basic ${token}`;
   axios.defaults.timeout = 4000;
   axios.interceptors.request.use(authInterceptor);
 }
