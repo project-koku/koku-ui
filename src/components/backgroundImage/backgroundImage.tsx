b7afd951d97bbbec2e9d12a4678d7a7433ce6050
Dan Labrecque <dlabrecq@redhat.com>
2018-11-09 12:25:25 -0500

    Replace custom background image with PF4 component
    Fixes https://github.com/project-koku/koku-ui/issues/182

--- src/components/backgroundImage/backgroundImage.tsx
+++ src/components/backgroundImage/backgroundImage.tsx
@@ -1,7 +1,25 @@
-import { css } from '@patternfly/react-styles';
+import {
+  BackgroundImage as Background,
+  BackgroundImageSrc,
+} from '@patternfly/react-core';
 import React from 'react';
-import { styles } from './backgroundImage.styles';
 
-const BackgroundImage = () => <div className={css(styles.backgroundImage)} />;
+/**
+ * Note: When using background-filter.svg, you must also include #image_overlay as the fragment identifier
+ */
+const images = {
+  [BackgroundImageSrc.lg]: '../../assets/pfbg_1200.jpg',
+  [BackgroundImageSrc.md]: '../../assets/pfbg_992.jpg',
+  [BackgroundImageSrc.md2x]: '../../assets/pfbg_992@2x.jpg',
+  [BackgroundImageSrc.sm]: '../../assets/pfbg_768.jpg',
+  [BackgroundImageSrc.sm2x]: '../../assets/pfbg_768@2x.jpg',
+  [BackgroundImageSrc.xl]: '../../assets/pfbg_2000.jpg',
+  [BackgroundImageSrc.xs]: '../../assets/pfbg_576.jpg',
+  [BackgroundImageSrc.xs2x]: '../../assets/pfbg_576@2x.jpg',
+  [BackgroundImageSrc.filter]:
+    '../../assets/background-filter.svg#image_overlay',
+};
+
+const BackgroundImage = () => <Background src={images} />;
 
 export { BackgroundImage };
