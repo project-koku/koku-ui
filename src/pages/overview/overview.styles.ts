51a45adc4c01f708cd4d01509ff8e907ad115f83
Michael Coker <mcoker@redhat.com>
2018-11-12 09:42:26 -0600

    chore(overview): replace custom CSS with utility classes

--- src/pages/overview/overview.styles.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from '@patternfly/react-styles';
-
-export const styles = StyleSheet.create({
-  banner: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-});
--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,10 @@ module.exports = env => {
     entry: [
       require.resolve('patternfly/dist/css/patternfly.css'),
       require.resolve('patternfly/dist/css/patternfly-additions.css'),
+      require.resolve(
+        '@patternfly/patternfly-next/utilities/Display/display.css'
+      ),
+      require.resolve('@patternfly/patternfly-next/utilities/Flex/flex.css'),
       path.join(srcDir, './styles/global.css'),
       path.join(srcDir, 'index.tsx'),
     ],
