5f5ca5542afd24199353802e7f64a324a13df755
Dan Labrecque <dlabrecq@redhat.com>
2019-03-14 10:08:11 -0400

    Update API for OCP on AWS cost card
    Fixes https://github.com/project-koku/koku-ui/issues/612

--- src/api/ocpOnAwsReports.test.ts
+++ src/api/ocpOnAwsReports.test.ts
@@ -6,5 +6,7 @@ import { OcpOnAwsReportType, runReport } from './ocpOnAwsReports';
 test('api run reports calls axios get', () => {
   const query = 'filter[resolution]=daily';
   runReport(OcpOnAwsReportType.cost, query);
-  expect(axios.get).toBeCalledWith(`reports/openshift/costs/?${query}`);
+  expect(axios.get).toBeCalledWith(
+    `reports/openshift/infrastructures/aws/costs/?${query}`
+  );
 });
