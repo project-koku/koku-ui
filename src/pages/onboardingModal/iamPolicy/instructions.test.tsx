ab879b7f4d5e1e04f20482abf9fae7efe597c5ac
Boaz Shuster <boaz.shuster.github@gmail.com>
2019-05-30 13:27:00 +0300

    refactor(onboarding): use the clipboard copy component from pf-4
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- src/pages/onboardingModal/iamPolicy/instructions.test.tsx
+++ src/pages/onboardingModal/iamPolicy/instructions.test.tsx
@@ -1,4 +1,4 @@
-import CopyClipboard from 'components/copyClipboard';
+import { ClipboardCopy } from '@patternfly/react-core';
 import { shallow } from 'enzyme';
 import React from 'react';
 import IamPolicyInstructions from './instructions';
@@ -8,8 +8,8 @@ test('copy clipboard has s3 bucket name', () => {
   const view = shallow(
     <IamPolicyInstructions t={jest.fn(v => v)} s3BucketName={bucketName} />
   );
-  const ccp = view.find(CopyClipboard);
+  const ccp = view.find(ClipboardCopy);
   expect(ccp).toHaveLength(1);
-  expect(ccp.props().text).toMatch(`"arn:aws:s3:::${bucketName}"`);
-  expect(ccp.props().text).toMatch(`"arn:aws:s3:::${bucketName}/*"`);
+  expect(ccp.props().children).toMatch(`"arn:aws:s3:::${bucketName}"`);
+  expect(ccp.props().children).toMatch(`"arn:aws:s3:::${bucketName}/*"`);
 });
