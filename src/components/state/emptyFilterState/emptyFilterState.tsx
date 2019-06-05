806b8575fe954f0942086ccb9a47d9e4ebd93afd
Dan Labrecque <dlabrecq@redhat.com>
2019-06-05 14:22:09 -0400

    Updated empty filter state for use with sources
    Fixes https://github.com/project-koku/koku-ui/issues/893

--- src/components/state/emptyFilterState/emptyFilterState.tsx
+++ src/components/state/emptyFilterState/emptyFilterState.tsx
@@ -2,49 +2,83 @@ import {
   EmptyState,
   EmptyStateBody,
   EmptyStateIcon,
+  Title,
 } from '@patternfly/react-core';
-import { ExclamationTriangleIcon } from '@patternfly/react-icons';
+import { SearchIcon } from '@patternfly/react-icons';
+import { css } from '@patternfly/react-styles';
 import { OcpOnAwsQuery, parseQuery } from 'api/ocpOnAwsQuery';
 import React from 'react';
 import { InjectedTranslateProps, translate } from 'react-i18next';
 import { styles } from './emptyFilterState.styles';
 
 interface EmptyFilterStateProps extends InjectedTranslateProps {
-  showAltIcon?: boolean;
+  filter?: string;
   icon?: React.ReactNode;
+  showMargin?: boolean;
+  subTitle?: string;
+  title?: string;
 }
 
 const EmptyFilterStateBase: React.SFC<EmptyFilterStateProps> = ({
-  icon = ExclamationTriangleIcon,
+  filter,
+  icon = SearchIcon,
+  showMargin = true,
   t,
+
+  // destructure last
+  subTitle = t('empty_filter_state.subtitle'),
+  title = t('empty_filter_state.title'),
 }) => {
-  const queryFromRoute = parseQuery<OcpOnAwsQuery>(location.search);
-  const testVal = val => val === 'koku';
-  let showAltIcon = false;
-  for (const values of Object.values(queryFromRoute.group_by)) {
-    if (Array.isArray(values)) {
-      for (const val of values) {
-        if (testVal(val)) {
+  const getIcon = () => {
+    const filterTest = val => val === atob('a29rdQ==');
+    let showAltIcon = false;
+    if (filter) {
+      for (const val of filter.split(',')) {
+        if (filterTest(val)) {
           showAltIcon = true;
           break;
         }
       }
     } else {
-      if (testVal(values)) {
-        showAltIcon = true;
-        break;
+      const queryFromRoute = parseQuery<OcpOnAwsQuery>(location.search);
+      if (queryFromRoute && queryFromRoute.group_by) {
+        for (const values of Object.values(queryFromRoute.group_by)) {
+          if (Array.isArray(values)) {
+            for (const val of values) {
+              if (filterTest(val)) {
+                showAltIcon = true;
+                break;
+              }
+            }
+          } else {
+            if (filterTest(values)) {
+              showAltIcon = true;
+              break;
+            }
+          }
+        }
       }
     }
-  }
+    if (showAltIcon) {
+      return <img className={styles.icon} />;
+    } else {
+      return <EmptyStateIcon icon={icon} />;
+    }
+  };
+
   return (
-    <EmptyState>
-      {Boolean(showAltIcon) ? (
-        <img className={styles.icon} />
-      ) : (
-        <EmptyStateIcon icon={icon} />
+    <div
+      className={css(
+        styles.container,
+        showMargin ? styles.containerMargin : ''
       )}
-      <EmptyStateBody>{t('empty_filter_state.subtitle')}</EmptyStateBody>
-    </EmptyState>
+    >
+      <EmptyState>
+        {getIcon()}
+        <Title size="lg">{title}</Title>
+        <EmptyStateBody>{subTitle}</EmptyStateBody>
+      </EmptyState>
+    </div>
   );
 };
 
