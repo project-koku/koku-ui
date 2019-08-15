7cccb78aea04a94c12a7ffca5dc702b30b2c9451
Anu <aagarw10@ncsu.edu>
2019-08-15 15:06:46 -0400

    updated bullet chart colors

--- src/components/charts/bulletChart/bulletChart.styles.ts
+++ src/components/charts/bulletChart/bulletChart.styles.ts
@@ -3,6 +3,16 @@ import {
   global_danger_color_100,
   global_spacer_md,
   global_spacer_sm,
+  chart_color_black_100,
+  chart_color_black_200,
+  chart_color_black_300,
+  chart_color_black_400,
+  chart_color_black_500,
+  chart_color_blue_100,
+  chart_color_blue_200,
+  chart_color_blue_300,
+  chart_color_blue_400,
+  chart_color_blue_500
 } from '@patternfly/react-tokens';
 
 export const chartStyles = {
@@ -11,24 +21,34 @@ export const chartStyles = {
   legendHeight: 30,
   // See: https://github.com/project-koku/koku-ui/issues/241
   rangeColorScale: [
-    '#ededed',
-    '#d1d1d1',
-    '#bbb',
-    '#72767b',
-    '#72767b',
-    '#282d33',
+    // '#ededed',
+    // '#d1d1d1',
+    // '#bbb',
+    // '#72767b',
+    // '#72767b',
+    // '#282d33',
+    chart_color_black_100.value,
+    chart_color_black_200.value,
+    chart_color_black_300.value,
+    chart_color_black_400.value,
+    chart_color_black_500.value
   ],
   rangeWidth: 40,
   thresholdErrorColor: global_danger_color_100.value,
   thresholdErrorWidth: 2,
   // See: https://github.com/project-koku/koku-ui/issues/241
   valueColorScale: [
-    '#007BBA',
-    '#bee1f4',
-    '#7DC3E8',
-    '#39A5DC',
-    '#00659C',
-    '#004D76',
+    // '#007BBA',
+    // '#bee1f4',
+    // '#7DC3E8',
+    // '#39A5DC',
+    // '#00659C',
+    // '#004D76',
+    chart_color_blue_100.value,
+    chart_color_blue_200.value,
+    chart_color_blue_300.value,
+    chart_color_blue_400.value,
+    chart_color_blue_500.value
   ],
   valueWidth: 9,
 };
