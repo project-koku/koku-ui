585172f042ffd249ac04494cbf757fae30e86860
Boaz Shuster <boaz.shuster.github@gmail.com>
2019-11-14 14:29:01 +0200

    fix(costmodels): Add timeout to markup validation in the wizard
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- src/pages/createCostModelWizard/markup.tsx
+++ src/pages/createCostModelWizard/markup.tsx
@@ -15,51 +15,78 @@ import React from 'react';
 import { InjectedTranslateProps, translate } from 'react-i18next';
 import { CostModelContext } from './context';
 
-const Markup: React.SFC<InjectedTranslateProps> = ({ t }) => (
-  <CostModelContext.Consumer>
-    {({ onMarkupChange, markup }) => {
-      const isValidMarkup = !isNaN(Number(markup));
-      return (
-        <Stack gutter="md">
-          <StackItem>
-            <Title size="xl">{t('cost_models_wizard.markup.title')}</Title>
-          </StackItem>
-          <StackItem>
-            <TextContent>
-              <Text component={TextVariants.h6}>
-                {t('cost_models_wizard.markup.sub_title')}
-              </Text>
-            </TextContent>
-          </StackItem>
-          <StackItem>
-            <Form>
-              <FormGroup
-                label={t('cost_models_wizard.markup.markup_label')}
-                fieldId="markup"
-                helperTextInvalid={t(
-                  'cost_models_wizard.markup.invalid_markup_text'
-                )}
-                isValid={isValidMarkup}
-              >
-                <InputGroup style={{ width: '150px' }}>
-                  <TextInput
-                    type="text"
-                    id="markup"
-                    name="markup"
-                    value={markup}
-                    onChange={onMarkupChange}
-                    isValid={isValidMarkup}
-                    placeholder={'0'}
-                  />
-                  <InputGroupText style={{ borderLeft: '0' }}>%</InputGroupText>
-                </InputGroup>
-              </FormGroup>
-            </Form>
-          </StackItem>
-        </Stack>
-      );
-    }}
-  </CostModelContext.Consumer>
-);
+interface MarkupValidationState {
+  isValid: boolean;
+}
+
+class Markup extends React.Component<
+  InjectedTranslateProps,
+  MarkupValidationState
+> {
+  public state = {
+    isValid: true,
+  };
+
+  public render() {
+    const { t } = this.props;
+    const { isValid } = this.state;
+
+    return (
+      <CostModelContext.Consumer>
+        {({ onMarkupChange, markup }) => {
+          return (
+            <Stack gutter="md">
+              <StackItem>
+                <Title size="xl">{t('cost_models_wizard.markup.title')}</Title>
+              </StackItem>
+              <StackItem>
+                <TextContent>
+                  <Text component={TextVariants.h6}>
+                    {t('cost_models_wizard.markup.sub_title')}
+                  </Text>
+                </TextContent>
+              </StackItem>
+              <StackItem>
+                <Form>
+                  <FormGroup
+                    label={t('cost_models_wizard.markup.markup_label')}
+                    fieldId="markup"
+                    helperTextInvalid={t(
+                      'cost_models_wizard.markup.invalid_markup_text'
+                    )}
+                    isValid={isValid}
+                  >
+                    <InputGroup style={{ width: '150px' }}>
+                      <TextInput
+                        type="text"
+                        id="markup"
+                        name="markup"
+                        value={markup}
+                        onBlur={() => {
+                          this.setState({ isValid: !isNaN(Number(markup)) });
+                        }}
+                        onChange={(value: string) => {
+                          onMarkupChange(value);
+                          if (value !== '-') {
+                            this.setState({ isValid: !isNaN(Number(value)) });
+                          }
+                        }}
+                        isValid={isValid}
+                        placeholder={'0'}
+                      />
+                      <InputGroupText style={{ borderLeft: '0' }}>
+                        %
+                      </InputGroupText>
+                    </InputGroup>
+                  </FormGroup>
+                </Form>
+              </StackItem>
+            </Stack>
+          );
+        }}
+      </CostModelContext.Consumer>
+    );
+  }
+}
 
 export default translate()(Markup);
