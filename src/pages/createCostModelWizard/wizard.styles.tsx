9375308c33402e5a5850f5389718827461abfd38
Boaz Shuster <boaz.shuster.github@gmail.com>
2019-12-15 10:13:47 +0200

    fix(costmodels): set min and max width/height to textarea
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- src/pages/createCostModelWizard/wizard.styles.tsx
+++ src/pages/createCostModelWizard/wizard.styles.tsx
@@ -4,4 +4,10 @@ export const styles = StyleSheet.create({
   form: {
     width: '350px',
   },
+  textArea: {
+    maxWidth: '450px',
+    minWidth: '350px',
+    minHeight: '75px',
+    maxHeight: '150px',
+  },
 });
