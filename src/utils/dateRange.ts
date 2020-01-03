8e3d77e0cdac5e5e4fd1b47c16b8e0b93a2c48d4
Dan Labrecque <dlabrecq@redhat.com>
2020-01-02 21:47:39 -0500

    Change Ocp cloud details delta to show previous cost
    Related to https://github.com/project-koku/koku-ui/issues/1093

--- src/utils/dateRange.ts
+++ src/utils/dateRange.ts
@@ -4,6 +4,29 @@ import getMonth from 'date-fns/get_month';
 import startOfMonth from 'date-fns/start_of_month';
 import i18next from 'i18next';
 
+export function getForDateRangeString(
+  value: string | number,
+  key: string = 'for_date',
+  offset: number = 1
+) {
+  const today = new Date();
+  if (offset) {
+    today.setMonth(today.getMonth() - offset);
+  }
+
+  const month = getMonth(today);
+  const endDate = formatDate(today, 'D');
+  const startDate = formatDate(startOfMonth(today), 'D');
+
+  return i18next.t(key, {
+    count: getDate(today),
+    endDate,
+    month,
+    startDate,
+    value,
+  });
+}
+
 export function getSinceDateRangeString(key: string = 'since_date') {
   const today = new Date();
   const month = getMonth(today);
