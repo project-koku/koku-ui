64c570a3f1115b20a5ff3ce873b33ebcdcc91fed
Boaz Shuster <boaz.shuster.github@gmail.com>
2020-02-04 17:39:32 +0200

    feat(costmodel): add the cancel button to add rate form in wizard
     - Did some refactoring to make it easier to test
     - Add empty state to price list table
     - Add the cancel button to the price list form
    
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- src/pages/costModelsDetails/components/addCostModelRateForm.test.tsx
+++ src/pages/costModelsDetails/components/addCostModelRateForm.test.tsx
@@ -1,19 +1,4 @@
-import { mount } from 'enzyme';
-import React from 'react';
-import {
-  AddCostModelRateFormBase,
-  isRateValid,
-  unusedRates,
-} from './addCostModelRateForm';
-
-test('isRateValid', () => {
-  ['2', '2.3', '200.34322123', '0.000123', '3.'].forEach(rate => {
-    expect(isRateValid(rate)).toBeTruthy();
-  });
-  ['-2', 'ab', '3.a', '-', '0'].forEach(rate => {
-    expect(isRateValid(rate)).toBeFalsy();
-  });
-});
+import { unusedRates } from './addCostModelRateForm';
 
 test('unusedRates', () => {
   const metricsHash = {
@@ -69,83 +54,5 @@ test('unusedRates', () => {
     a: { a: true, b: true },
     b: { a: true, c: true },
     c: { z: true },
-  });
-});
-
-describe('add cost model rate form', () => {
-  test('initial form', () => {
-    const view = mount(
-      <AddCostModelRateFormBase
-        t={(v: string) => v}
-        metric=""
-        setMetric={jest.fn()}
-        measurement=""
-        setMeasurement={jest.fn()}
-        rate=""
-        setRate={jest.fn()}
-        metricOptions={[
-          { label: 'm1', value: 'm1' },
-          { label: 'm2', value: 'm2' },
-        ]}
-        measurementOptions={[
-          { label: 'a1', value: 'a1' },
-          { label: 'a2', value: 'a2' },
-        ]}
-        validRate
-      />
-    );
-    expect(view.find('select')).toHaveLength(1);
-    expect(view.find('option')).toHaveLength(3);
-    expect(view.find('input')).toHaveLength(0);
-  });
-  test('metric selected', () => {
-    const view = mount(
-      <AddCostModelRateFormBase
-        t={(v: string) => v}
-        metric="m2"
-        setMetric={jest.fn()}
-        measurement=""
-        setMeasurement={jest.fn()}
-        rate=""
-        setRate={jest.fn()}
-        metricOptions={[
-          { label: 'm1', value: 'm1' },
-          { label: 'm2', value: 'm2' },
-        ]}
-        measurementOptions={[
-          { label: 'a1', value: 'a1' },
-          { label: 'a2', value: 'a2' },
-        ]}
-        validRate
-      />
-    );
-    expect(view.find('select')).toHaveLength(2);
-    expect(view.find('option')).toHaveLength(6);
-    expect(view.find('input')).toHaveLength(0);
-  });
-  test('measurement selected', () => {
-    const view = mount(
-      <AddCostModelRateFormBase
-        t={(v: string) => v}
-        metric="m2"
-        setMetric={jest.fn()}
-        measurement="a1"
-        setMeasurement={jest.fn()}
-        rate=""
-        setRate={jest.fn()}
-        metricOptions={[
-          { label: 'm1', value: 'm1' },
-          { label: 'm2', value: 'm2' },
-        ]}
-        measurementOptions={[
-          { label: 'a1', value: 'a1' },
-          { label: 'a2', value: 'a2' },
-        ]}
-        validRate
-      />
-    );
-    expect(view.find('select')).toHaveLength(2);
-    expect(view.find('option')).toHaveLength(6);
-    expect(view.find('input')).toHaveLength(1);
   });
 });
