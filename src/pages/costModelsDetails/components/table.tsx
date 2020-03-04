01a60b3bd94a2c286efa73ea79e1091da88033de
Dan Labrecque <dlabrecq@redhat.com>
2020-03-04 17:22:53 -0500

    feat(costmodels): migrate to pf4 datatoolbar in the sources tab (#1357)
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- src/pages/costModelsDetails/components/table.tsx
+++ src/pages/costModelsDetails/components/table.tsx
@@ -1,5 +1,4 @@
 import {
-  Button,
   EmptyState,
   EmptyStateBody,
   EmptyStateIcon,
@@ -11,13 +10,16 @@ import {
   ToolbarSection,
 } from '@patternfly/react-core';
 import { DollarSignIcon } from '@patternfly/react-icons';
+import { css } from '@patternfly/react-styles';
 import { Table, TableBody, TableHeader } from '@patternfly/react-table';
-import FilterComposition from 'components/filter/filterComposition';
-import FilterResults from 'components/filter/filterResults';
 import { EmptyFilterState } from 'components/state/emptyFilterState/emptyFilterState';
-import { css } from 'emotion';
 import React from 'react';
 import { InjectedTranslateProps, translate } from 'react-i18next';
+import {
+  addMultiValueQuery,
+  removeMultiValueQuery,
+} from '../../createCostModelWizard/filterLogic';
+import { SourcesToolbar } from './sourcesToolbar';
 import { styles } from './table.styles';
 
 interface Props extends InjectedTranslateProps {
@@ -25,15 +27,7 @@ interface Props extends InjectedTranslateProps {
   cells: string[];
   onDelete: (item: object) => void;
   onDeleteText?: string;
-  onAdd: {
-    onClick: () => void;
-    label: string;
-  };
-}
-
-interface CurrentFilter {
-  name: string;
-  value: string;
+  onAdd: () => void;
 }
 
 interface PaginationQuery {
@@ -42,35 +36,15 @@ interface PaginationQuery {
 }
 
 interface State {
-  query: { [k: string]: string };
-  currentFilter: CurrentFilter;
+  query: { name: string[] };
+  currentFilter: string;
   pagination: PaginationQuery;
 }
 
-const switchFilterType = (name: string) => {
-  return (nextFnc: (filter: CurrentFilter) => void) => {
-    return (currentFilter: CurrentFilter) => {
-      if (name === currentFilter.name) {
-        return;
-      }
-      nextFnc(currentFilter);
-    };
-  };
-};
-
-const setCurrentFilter = (
-  currentState: CurrentFilter,
-  setState: (obj: any) => void
-) => {
-  return (currentFilter: CurrentFilter) => {
-    setState({ currentFilter });
-  };
-};
-
 class TableBase extends React.Component<Props, State> {
   public state = {
-    query: { Name: '' },
-    currentFilter: { name: 'Name', value: '' },
+    query: { name: [] },
+    currentFilter: '',
     pagination: { page: 1, perPage: 10 },
   };
   public render() {
@@ -78,100 +52,73 @@ class TableBase extends React.Component<Props, State> {
       pagination: { page, perPage },
     } = this.state;
     const { onAdd, t, rows, cells } = this.props;
-    const changeBuffer = setCurrentFilter(
-      this.state.currentFilter,
-      this.setState.bind(this)
-    );
-    const changeType = switchFilterType(this.state.currentFilter.name)(
-      changeBuffer
-    );
     const filteredRows = rows
       .filter(uuid => {
-        const filters = this.state.query.Name.split(',');
-        return filters.every(fName => uuid.includes(fName));
+        if (!Boolean(this.state.query.name)) {
+          return true;
+        }
+        return this.state.query.name.every(fName => uuid.includes(fName));
       })
       .map(uuid => [uuid]);
     const res = filteredRows.slice((page - 1) * perPage, page * perPage);
     return (
       <>
-        <Toolbar>
-          <ToolbarSection
-            aria-label={t('cost_models_details.sources_filter_controller')}
-            style={{ justifyContent: 'space-between' }}
-          >
-            <ToolbarGroup>
-              <FilterComposition
-                isSingleOption
-                id={'costmodel-sources-table-filter'}
-                options={[
-                  { value: 'OCP', label: t('filter.type_ocp') },
-                  { value: 'AWS', label: t('filter.type_aws') },
-                ]}
-                query={this.state.query}
-                value={this.state.currentFilter.value}
-                name={this.state.currentFilter.name}
-                filters={['name']}
-                updateFilter={changeBuffer}
-                switchType={changeType}
-                onSearch={q => {
-                  this.setState({
-                    query: q,
-                    currentFilter: { ...this.state.currentFilter, value: '' },
-                    pagination: { ...this.state.pagination, page: 1 },
-                  });
-                }}
-              />
-              {onAdd && (
-                <ToolbarItem>
-                  <Button onClick={onAdd.onClick}>{onAdd.label}</Button>
-                </ToolbarItem>
-              )}
-            </ToolbarGroup>
-            <ToolbarGroup>
-              <ToolbarItem>
-                <Pagination
-                  itemCount={filteredRows.length}
-                  perPage={perPage}
-                  page={page}
-                  onSetPage={(_evt, newPage) =>
-                    this.setState({
-                      pagination: {
-                        ...this.state.pagination,
-                        page: newPage,
-                      },
-                    })
-                  }
-                  onPerPageSelect={(_evt, newPerPage) =>
-                    this.setState({
-                      pagination: { page: 1, perPage: newPerPage },
-                    })
-                  }
-                />
-              </ToolbarItem>
-            </ToolbarGroup>
-          </ToolbarSection>
-          <ToolbarSection
-            aria-label={t('cost_models_details.sources_filter_results')}
-          >
-            <FilterResults
-              query={this.state.query}
-              count={res.length}
-              onRemoveAll={() => {
-                this.setState({ query: { Name: '' } });
-              }}
-              onRemove={({ name, value }) => {
-                const curQuery = this.state.query[name];
-                if (curQuery === undefined) {
-                  return;
-                }
-                const newQuery = curQuery.split(',').filter(q => q !== value);
-                this.setState({
-                  query: { ...this.state.query, [name]: newQuery.join(',') },
-                });
-              }}
-            />
-          </ToolbarSection>
-        </Toolbar>
+        <SourcesToolbar
+          actionButtonProps={{
+            onClick: onAdd,
+            children: t('toolbar.sources.assign_sources'),
+          }}
+          filter={{
+            onClearAll: () =>
+              this.setState({
+                pagination: { ...this.state.pagination, page: 1 },
+                query: { name: [] },
+              }),
+            onRemove: (_category, chip) => {
+              this.setState({
+                pagination: { ...this.state.pagination, page: 1 },
+                query: removeMultiValueQuery(this.state.query)('name', chip),
+              });
+            },
+            query: this.state.query,
+            categoryNames: { name: t('toolbar.sources.category.name') },
+          }}
+          paginationProps={{
+            itemCount: filteredRows.length,
+            perPage,
+            page,
+            onSetPage: (_evt, newPage) =>
+              this.setState({
+                pagination: {
+                  ...this.state.pagination,
+                  page: newPage,
+                },
+              }),
+            onPerPageSelect: (_evt, newPerPage) =>
+              this.setState({
+                pagination: { page: 1, perPage: newPerPage },
+              }),
+          }}
+          searchInputProps={{
+            id: 'sources-tab-toolbar',
+            onChange: (value: string) =>
+              this.setState({
+                currentFilter: value,
+              }),
+            onSearch: () => {
+              this.setState({
+                query: addMultiValueQuery(this.state.query)(
+                  'name',
+                  this.state.currentFilter
+                ),
+                currentFilter: '',
+                pagination: { ...this.state.pagination, page: 1 },
+              });
+            },
+            value: this.state.currentFilter,
+            placeholder: t('toolbar.sources.filter_placeholder'),
+          }}
+        />
         {res.length > 0 && (
           <Table
             aria-label="cost-model-sources"
@@ -207,7 +154,7 @@ class TableBase extends React.Component<Props, State> {
         )}
         {filteredRows.length === 0 && rows.length > 0 && (
           <EmptyFilterState
-            filter={this.state.currentFilter.name}
+            filter={this.state.currentFilter}
             subTitle={t('no_match_found_state.desc')}
           />
         )}
