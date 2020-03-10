eddd1158e1aa90d64bfaa977a3301783e9f0efae
Dan Labrecque <dlabrecq@redhat.com>
2020-03-10 11:09:39 -0400

    Updated PatternFly to use the latest milestone packages
    https://github.com/project-koku/koku-ui/issues/1370

--- src/pages/details/ocpDetails/historicalModal.styles.ts
+++ src/pages/details/ocpDetails/historicalModal.styles.ts
@@ -1,15 +1,11 @@
-import { StyleSheet } from '@patternfly/react-styles';
 import { css } from 'emotion';
 
-export const styles = StyleSheet.create({
-  modal: {
-    // Workaround for isLarge not working properly
-    height: '900px',
-    width: '1200px',
-  },
-});
-
+// Workaround for isLarge not working properly
 export const modalOverride = css`
+  &.pf-c-modal-box {
+    height: '900px';
+    width: 1200px;
+  }
   & .pf-c-modal-box__footer {
     display: none;
   }
