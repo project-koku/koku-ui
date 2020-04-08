1bfbbf7116ad5022cc777f442c26fdd2ea37231a
Dan Labrecque <dlabrecq@redhat.com>
2020-04-07 22:06:17 -0400

    Progress bar margin
    https://github.com/project-koku/koku-ui/issues/1471

--- src/components/reports/reportSummary/reportSummaryItem.styles.ts
+++ src/components/reports/reportSummary/reportSummaryItem.styles.ts
@@ -2,7 +2,7 @@ import { global_spacer_md } from '@patternfly/react-tokens';
 import { css } from 'emotion';
 
 export const reportSummaryItem = css`
-  :not(:last-child): {
-    marginbottom: ${global_spacer_md.value};
+  :not(:last-child) {
+    margin-bottom: ${global_spacer_md.value};
   }
 `;
