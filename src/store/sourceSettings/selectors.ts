cfcb37e45a3b520503c4ddbb9e048329db2c479f
Boaz Shuster <boaz.shuster.github@gmail.com>
2020-06-24 13:59:21 +0300

    refactor(store): django pagination and query selectors
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- src/store/sourceSettings/selectors.ts
+++ src/store/sourceSettings/selectors.ts
@@ -1,3 +1,5 @@
+import { selectPagination } from 'store/djangoUtils/pagination';
+import { selectQuery } from 'store/djangoUtils/query';
 import { RootState } from 'store/rootReducer';
 import { stateKey } from './reducer';
 
@@ -21,66 +23,11 @@ export const currentFilterValue = (state: RootState) =>
 export const currentFilterType = (state: RootState) =>
   sourcesState(state).currentFilterType;
 
-export const query = (state: RootState) => {
-  const sourcesPayload = sourcesState(state).sources;
-  if (sourcesPayload === null) {
-    return {
-      name: null,
-      type: null,
-      offset: null,
-      limit: null,
-    };
-  }
-  const urlParams = new URLSearchParams(
-    sourcesPayload.links.first.split('?')[1]
-  );
-  return {
-    name: urlParams.get('name'),
-    type: urlParams.get('type'),
-    offset: urlParams.get('offset'),
-    limit: urlParams.get('limit'),
-  };
-};
+export const query = selectQuery(
+  (state: RootState) => sourcesState(state).sources,
+  ['name', 'type', 'offset', 'limit']
+);
 
-export const pagination = (state: RootState) => {
-  const sourcesPayload = sourcesState(state).sources;
-  if (sourcesPayload === null) {
-    return {
-      page: 1,
-      perPage: 1,
-      count: 0,
-    };
-  }
-
-  let urlParams = null;
-  if (sourcesPayload.links.next !== null) {
-    urlParams = new URLSearchParams(sourcesPayload.links.next.split('?')[1]);
-    const limit = Number(urlParams.get('limit'));
-    const offset = Number(urlParams.get('offset')) - limit;
-    return {
-      page: offset / limit + 1,
-      perPage: limit,
-      count: sourcesPayload.meta.count,
-    };
-  }
-
-  if (sourcesPayload.links.previous !== null) {
-    urlParams = new URLSearchParams(
-      sourcesPayload.links.previous.split('?')[1]
-    );
-    const limit = Number(urlParams.get('limit'));
-    const offset = Number(urlParams.get('offset')) + limit;
-    return {
-      page: offset / limit + 1,
-      perPage: limit,
-      count: sourcesPayload.meta.count,
-    };
-  }
-
-  urlParams = new URLSearchParams(sourcesPayload.links.first.split('?')[1]);
-  return {
-    page: 1,
-    perPage: Number(urlParams.get('limit')),
-    count: sourcesPayload.meta.count,
-  };
-};
+export const pagination = selectPagination(
+  (state: RootState) => sourcesState(state).sources
+);
