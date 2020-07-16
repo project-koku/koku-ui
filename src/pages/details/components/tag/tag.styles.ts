8e8bacf6c8922e310fd63633275e1609ae232d3d
Dan Labrecque <dlabrecq@redhat.com>
2020-07-16 15:35:26 -0400

    Tag list modal consistency update
    https://issues.redhat.com/browse/COST-134

--- src/pages/details/components/tag/tag.styles.ts
+++ src/pages/details/components/tag/tag.styles.ts
@@ -1,7 +1,13 @@
-import { global_spacer_3xl, global_spacer_sm } from '@patternfly/react-tokens';
+import { global_FontWeight_bold, global_spacer_3xl, global_spacer_lg, global_spacer_sm } from '@patternfly/react-tokens';
 import React from 'react';
 
 export const styles = {
+  dataListHeading: {
+    fontWeight: global_FontWeight_bold.value as any
+  },
+  groupByHeading: {
+    marginBottom: global_spacer_lg.value
+  },
   tagLink: {
     marginLeft: global_spacer_sm.value,
   },
--- src/pages/details/components/tag/tagModal.tsx
+++ src/pages/details/components/tag/tagModal.tsx
@@ -1,8 +1,11 @@
 import { Modal } from '@patternfly/react-core';
-import { ReportPathsType } from 'api/reports/report';
+import { getQuery, parseQuery, Query } from 'api/queries/query';
+import { Report, ReportPathsType, ReportType } from 'api/reports/report';
 import React from 'react';
 import { InjectedTranslateProps, translate } from 'react-i18next';
-import { modalOverride } from './tagModal.styles';
+import { connect } from 'react-redux';
+import { createMapStateToProps, FetchStatus } from 'store/common';
+import { reportActions, reportSelectors } from 'store/reports';
 import { TagView } from './tagView';
 
 interface TagModalOwnProps {
@@ -13,7 +16,22 @@ interface TagModalOwnProps {
   reportPathsType: ReportPathsType;
 }
 
-type TagModalProps = TagModalOwnProps & InjectedTranslateProps;
+interface TagModalStateProps {
+  queryString?: string;
+  report?: Report;
+  reportFetchStatus?: FetchStatus;
+}
+
+interface TagModalDispatchProps {
+  fetchReport?: typeof reportActions.fetchReport;
+}
+
+type TagModalProps = TagModalOwnProps &
+  TagModalStateProps &
+  TagModalDispatchProps &
+  InjectedTranslateProps;
+
+const reportType = ReportType.tag;
 
 class TagModalBase extends React.Component<TagModalProps> {
   constructor(props: TagModalProps) {
@@ -21,39 +39,102 @@ class TagModalBase extends React.Component<TagModalProps> {
     this.handleClose = this.handleClose.bind(this);
   }
 
+  public componentDidMount() {
+    const { fetchReport, queryString, reportPathsType } = this.props;
+    fetchReport(reportPathsType, reportType, queryString);
+  }
+
+  public componentDidUpdate(prevProps: TagModalProps) {
+    const { fetchReport, queryString, reportPathsType } = this.props;
+    if (prevProps.queryString !== queryString) {
+      fetchReport(reportPathsType, reportType, queryString);
+    }
+  }
+
   public shouldComponentUpdate(nextProps: TagModalProps) {
     const { filterBy, isOpen } = this.props;
     return nextProps.filterBy !== filterBy || nextProps.isOpen !== isOpen;
   }
+
+  private getTagValueCount = () => {
+    const { report } = this.props;
+    let count = 0;
+
+    if (report) {
+      for (const tag of report.data) {
+        for (const {} of tag.values) {
+          count++;
+        }
+      }
+    }
+    return count;
+  };
 
   private handleClose = () => {
     this.props.onClose(false);
   };
 
   public render() {
-    const { filterBy, groupBy, isOpen, reportPathsType, t } = this.props;
+    const { filterBy, groupBy, isOpen, report, t } = this.props;
 
     return (
       <Modal
-        className={modalOverride}
         isOpen={isOpen}
         onClose={this.handleClose}
-        title={t('details.tags_modal_title', {
-          groupBy,
-          name: filterBy,
+        title={t('tag.title', {
+          value: this.getTagValueCount()
         })}
         width={'50%'}
       >
         <TagView
           filterBy={filterBy}
           groupBy={groupBy}
-          reportPathsType={reportPathsType}
+          report={report}
         />
       </Modal>
     );
   }
 }
 
-const TagModal = translate()(TagModalBase);
+const mapStateToProps = createMapStateToProps<
+  TagModalOwnProps,
+  TagModalStateProps
+  >((state, { filterBy, groupBy, reportPathsType }) => {
+  const queryFromRoute = parseQuery<Query>(location.search);
+  const queryString = getQuery({
+    filter: {
+      [groupBy]: filterBy,
+      resolution: 'monthly',
+      time_scope_units: 'month',
+      time_scope_value: -1,
+      ...(queryFromRoute.filter.account && {account: queryFromRoute.filter.account})
+    },
+  });
+  const report = reportSelectors.selectReport(
+    state,
+    reportPathsType,
+    reportType,
+    queryString
+  );
+  const reportFetchStatus = reportSelectors.selectReportFetchStatus(
+    state,
+    reportPathsType,
+    reportType,
+    queryString
+  );
+  return {
+    queryString,
+    report,
+    reportFetchStatus,
+  };
+});
+
+const mapDispatchToProps: TagModalDispatchProps = {
+  fetchReport: reportActions.fetchReport,
+};
+
+const TagModal = translate()(
+  connect(mapStateToProps, mapDispatchToProps)(TagModalBase)
+);
 
 export { TagModal };
