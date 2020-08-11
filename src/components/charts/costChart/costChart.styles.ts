03acde5cf813aadcb042c4f319b6df1035ebbcd9
Dan Labrecque <dlabrecq@redhat.com>
2020-08-11 11:32:11 -0400

    Updated to latest patternfly and frontend-components milestones

--- src/components/charts/costChart/costChart.styles.ts
+++ src/components/charts/costChart/costChart.styles.ts
@@ -1,9 +1,9 @@
-import { chart_color_green_100 } from '@patternfly/react-tokens/dist/esm/chart_color_green_100';
-import { chart_color_green_200 } from '@patternfly/react-tokens/dist/esm/chart_color_green_200';
-import { chart_color_green_300 } from '@patternfly/react-tokens/dist/esm/chart_color_green_300';
-import { chart_color_green_400 } from '@patternfly/react-tokens/dist/esm/chart_color_green_400';
-import { chart_color_green_500 } from '@patternfly/react-tokens/dist/esm/chart_color_green_500';
-import { global_disabled_color_200 } from '@patternfly/react-tokens/dist/esm/global_disabled_color_200';
+import { chart_color_green_100 } from '@patternfly/react-tokens/dist/js/chart_color_green_100';
+import { chart_color_green_200 } from '@patternfly/react-tokens/dist/js/chart_color_green_200';
+import { chart_color_green_300 } from '@patternfly/react-tokens/dist/js/chart_color_green_300';
+import { chart_color_green_400 } from '@patternfly/react-tokens/dist/js/chart_color_green_400';
+import { chart_color_green_500 } from '@patternfly/react-tokens/dist/js/chart_color_green_500';
+import { global_disabled_color_200 } from '@patternfly/react-tokens/dist/js/global_disabled_color_200';
 
 export const chartStyles = {
   // See: https://github.com/project-koku/koku-ui/issues/241
