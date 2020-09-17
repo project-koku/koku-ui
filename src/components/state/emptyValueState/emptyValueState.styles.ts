8d52217fe5f7b4f25ecbe381dbe28a627658425a
Doug Donahue <57504257+ddonahue007@users.noreply.github.com>
2020-09-17 13:14:01 -0400

    Remove emotion 3 (#1664)
    * remove emotion from emptyValueState
    
    * remove emotion from emptyFilterState
    
    * fixed prettier errors...

--- src/components/state/emptyValueState/emptyValueState.styles.ts
+++
@@ -1,8 +0,0 @@
-import global_FontSize_sm from '@patternfly/react-tokens/dist/js/global_FontSize_sm';
-import React from 'react';
-
-export const styles = {
-  container: {
-    fontSize: global_FontSize_sm.value,
-  },
-} as { [className: string]: React.CSSProperties };
