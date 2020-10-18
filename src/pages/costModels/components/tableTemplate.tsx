8ed1536763555683b2872bbc187026226084a40f
Boaz Shuster <boaz.shuster.github@gmail.com>
2020-10-18 11:55:34 +0300

    refactor(costmodels): use table template in tag rate table
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- src/pages/costModels/components/tableTemplate.tsx
+++ src/pages/costModels/components/tableTemplate.tsx
@@ -1,9 +1,9 @@
 import { Table, TableBody, TableGridBreakpoint, TableHeader, TableProps } from '@patternfly/react-table';
 import { TFunction } from 'i18next';
 import React from 'react';
-import { Translation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
-export type TableTemplateProps = Pick<TableProps, 'aria-label' | 'actions' | 'cells' | 'rows' | 'onSort' | 'sortBy'>;
+export type TableTemplateProps = TableProps;
 
 export const translateTableProps = (t: TFunction, props: TableTemplateProps) => {
   const cells = props.cells.map(cell => {
@@ -15,16 +15,18 @@ export const translateTableProps = (t: TFunction, props: TableTemplateProps) => 
     }
     return cell;
   });
-  const actions = props.actions.map(action => {
-    let act = { ...action };
-    if (typeof action.title === 'string') {
-      act = { ...act, title: t(action.title) };
-    }
-    if (action.tooltip && typeof action.tooltip === 'string') {
-      act = { ...act, tooltip: t(action.tooltip) };
-    }
-    return act;
-  });
+  const actions = props.actions
+    ? props.actions.map(action => {
+        let act = { ...action };
+        if (typeof action.title === 'string') {
+          act = { ...act, title: t(action.title) };
+        }
+        if (action.tooltip && typeof action.tooltip === 'string') {
+          act = { ...act, tooltip: t(action.tooltip) };
+        }
+        return act;
+      })
+    : null;
   return {
     ...props,
     cells,
@@ -33,26 +35,11 @@ export const translateTableProps = (t: TFunction, props: TableTemplateProps) => 
 };
 
 export const TableTemplate: React.FunctionComponent<TableTemplateProps> = props => {
+  const { t } = useTranslation();
   return (
-    <Translation>
-      {t => {
-        const translatedProps = translateTableProps(t, props);
-        const { actions, cells, rows, onSort, sortBy, 'aria-label': ariaLabel } = translatedProps;
-        return (
-          <Table
-            aria-label={ariaLabel}
-            gridBreakPoint={TableGridBreakpoint.grid2xl}
-            cells={cells}
-            rows={rows}
-            sortBy={sortBy}
-            onSort={onSort}
-            actions={actions}
-          >
-            <TableHeader />
-            <TableBody />
-          </Table>
-        );
-      }}
-    </Translation>
+    <Table gridBreakPoint={TableGridBreakpoint.grid2xl} {...translateTableProps(t, props)}>
+      <TableHeader />
+      <TableBody />
+    </Table>
   );
 };
--- src/pages/costModels/components/tagRateTable.tsx
+++ src/pages/costModels/components/tagRateTable.tsx
@@ -1,14 +1,41 @@
-import { Table, TableBody, TableHeader, TableVariant } from '@patternfly/react-table';
+import { IRow, TableVariant } from '@patternfly/react-table';
 import { TagRates } from 'api/rates';
+import { TFunction } from 'i18next';
+import { TableTemplate } from 'pages/costModels/components/tableTemplate';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { formatCurrency } from 'utils/formatValue';
 
 interface TagRateTableProps {
   tagRates: TagRates;
 }
 
+const cells = [
+  'cost_models.table.tag_key',
+  'cost_models.table.tag_value',
+  'cost_models.table.rate',
+  'cost_models.table.description',
+  'cost_models.table.default',
+];
+
+const translateRows = (t: TFunction, rows: IRow[]): IRow[] => {
+  return rows.map(row => {
+    const rowCells = row.cells.map(cell => {
+      if (typeof cell === 'string') {
+        return t(cell);
+      }
+      return cell;
+    });
+
+    return {
+      ...row,
+      cells: rowCells,
+    };
+  });
+};
+
 const TagRateTable: React.FunctionComponent<TagRateTableProps> = ({ tagRates }) => {
-  const cells = ['Tag key', 'Tag value', 'Rate', 'Description', 'Default'];
+  const { t } = useTranslation();
   const rows = tagRates.tag_values.map((tagValue, ix) => {
     return {
       cells: [
@@ -16,21 +43,19 @@ const TagRateTable: React.FunctionComponent<TagRateTableProps> = ({ tagRates }) 
         tagValue.tag_value,
         formatCurrency(tagValue.value),
         tagValue.description,
-        tagValue.default ? 'Yes' : 'No',
+        tagValue.default ? 'cost_models.table.default_yes' : 'cost_models.table.default_no',
       ],
     };
   });
+  const translatedRows = translateRows(t, rows);
   return (
-    <Table
+    <TableTemplate
       aria-label={`tag-table-rate-${tagRates.tag_key}`}
-      variant={TableVariant.compact}
       borders={false}
+      variant={TableVariant.compact}
       cells={cells}
-      rows={rows}
-    >
-      <TableHeader />
-      <TableBody />
-    </Table>
+      rows={translatedRows}
+    />
   );
 };
 
