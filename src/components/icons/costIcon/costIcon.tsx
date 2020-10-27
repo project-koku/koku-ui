9e77b890ba94c1e93c2fc011dc201b7ba5128561
Dan Labrecque <dlabrecq@redhat.com>
2020-10-27 09:00:01 -0400

    Added aria-hidden and alt text to cost icon

--- src/components/icons/costIcon/costIcon.tsx
+++ src/components/icons/costIcon/costIcon.tsx
@@ -1,15 +1,16 @@
 import './costIcon.scss';
 
 import React from 'react';
+import { WithTranslation, withTranslation } from 'react-i18next';
 
-interface CostIconProps {
+interface CostIconProps extends WithTranslation {
   className?: string;
 }
 
 const icon = require('./Cost-icon.svg');
 
-const CostIcon: React.SFC<CostIconProps> = ({ className }) => {
-  return <img className={`cost-icon ${className}`} src={icon} />;
+const CostIcon: React.SFC<CostIconProps> = ({ className, t }) => {
+  return <img className={`cost-icon ${className}`} src={icon} alt={t('cost_management')} aria-hidden="true" />;
 };
 
-export default CostIcon;
+export default withTranslation()(CostIcon);
