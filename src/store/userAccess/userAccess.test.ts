68334dcf10001f06f7d912e695bd6b11b0922093
Dan Labrecque <dlabrecq@redhat.com>
2021-01-25 18:58:17 -0500

    Dynamically hide Insights navigation links
    https://issues.redhat.com/browse/COST-901

---
+++ src/store/userAccess/userAccess.test.ts
@@ -0,0 +1,62 @@
+jest.mock('api/userAccess');
+
+import { getUserAccessQuery } from 'api/queries/userAccessQuery';
+import { fetchUserAccess, UserAccess, UserAccessType } from 'api/userAccess';
+import { FetchStatus } from 'store/common';
+import { createMockStoreCreator } from 'store/mockStore';
+import { wait } from 'testUtils';
+
+import * as actions from './userAccessActions';
+import { awsUserAccessQuery, stateKey } from './userAccessCommon';
+import { userAccessReducer } from './userAccessReducer';
+import * as selectors from './userAccessSelectors';
+
+const createProdvidersStore = createMockStoreCreator({
+  [stateKey]: userAccessReducer,
+});
+
+const fetchUserAccessMock = fetchUserAccess as jest.Mock;
+
+const userAccessMock: UserAccess = {
+  data: [
+    {
+      access: true,
+      type: 'aws',
+    },
+  ],
+};
+
+fetchUserAccessMock.mockReturnValue(Promise.resolve({ data: userAccessMock }));
+
+test('default state', async () => {
+  const store = createProdvidersStore();
+  expect(selectors.selectUserAccessState(store.getState())).toMatchSnapshot();
+});
+
+test('fetch userAccess success', async () => {
+  const query = getUserAccessQuery(awsUserAccessQuery);
+  const store = createProdvidersStore();
+  store.dispatch(actions.fetchUserAccess(UserAccessType.aws, query));
+  expect(fetchUserAccessMock).toBeCalled();
+  expect(selectors.selectUserAccessFetchStatus(store.getState(), UserAccessType.aws, query)).toBe(
+    FetchStatus.inProgress
+  );
+  await wait();
+  const finishedState = store.getState();
+  expect(selectors.selectUserAccessFetchStatus(finishedState, UserAccessType.aws, query)).toBe(FetchStatus.complete);
+});
+
+test('fetch userAccess failure', async () => {
+  const query = getUserAccessQuery(awsUserAccessQuery);
+  const store = createProdvidersStore();
+  const error = Symbol('getUserAccess error');
+  fetchUserAccessMock.mockReturnValueOnce(Promise.reject(error));
+  store.dispatch(actions.fetchUserAccess(UserAccessType.aws, query));
+  expect(fetchUserAccessMock).toBeCalled();
+  expect(selectors.selectUserAccessFetchStatus(store.getState(), UserAccessType.aws, query)).toBe(
+    FetchStatus.inProgress
+  );
+  await wait();
+  const finishedState = store.getState();
+  expect(selectors.selectUserAccessFetchStatus(finishedState, UserAccessType.aws, query)).toBe(FetchStatus.complete);
+});
---
+++ src/store/userAccess/userAccessCommon.ts
@@ -0,0 +1,33 @@
+import { UserAccessQuery } from 'api/queries/userAccessQuery';
+import { UserAccessType } from 'api/userAccess';
+
+export const stateKey = 'userAccess';
+export const userAccessKey = 'user-access';
+
+export const allUserAccessQuery: UserAccessQuery = {
+  type: '',
+};
+
+export const awsUserAccessQuery: UserAccessQuery = {
+  type: 'AWS',
+};
+
+export const azureUserAccessQuery: UserAccessQuery = {
+  type: 'AZURE',
+};
+
+export const costModelUserAccessQuery: UserAccessQuery = {
+  type: 'cost_model',
+};
+
+export const ocpUserAccessQuery: UserAccessQuery = {
+  type: 'OCP',
+};
+
+export const gcpUserAccessQuery: UserAccessQuery = {
+  type: 'GCP',
+};
+
+export function getReportId(type: UserAccessType, query: string) {
+  return `${type}--${query}`;
+}
