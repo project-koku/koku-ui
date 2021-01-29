5463436696a83bf9533fdf31f0e47332eec76e24
Doug Donahue <ddonahue@redhat.com>
2021-01-29 09:07:04 -0500

    add forecast modeling for GCP

--- src/pages/dashboard/gcpDashboard/gcpDashboardWidget.tsx
+++ src/pages/dashboard/gcpDashboard/gcpDashboardWidget.tsx
@@ -7,10 +7,14 @@ import { withTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 import { createMapStateToProps } from 'store/common';
 import { gcpDashboardActions, gcpDashboardSelectors, GcpDashboardTab } from 'store/dashboard/gcpDashboard';
+import { forecastSelectors } from 'store/forecasts';
 import { reportSelectors } from 'store/reports';
 import { ComputedGcpReportItemsParams } from 'utils/computedReport/getComputedGcpReportItems';
 
+import { awsDashboardActions } from '../../../store/dashboard/awsDashboard';
+
 interface GcpDashboardWidgetDispatchProps {
+  fetchForecasts: typeof awsDashboardActions.fetchWidgetForecasts;
   fetchReports: typeof gcpDashboardActions.fetchWidgetReports;
   updateTab: typeof gcpDashboardActions.changeWidgetTab;
 }
@@ -45,6 +49,12 @@ const mapStateToProps = createMapStateToProps<DashboardWidgetOwnProps, Dashboard
         widget.reportType,
         queries.current
       ),
+      forecast: forecastSelectors.selectForecast(
+        state,
+        widget.forecastPathsType,
+        widget.forecastType,
+        queries.forecast
+      ),
       previousReport: reportSelectors.selectReport(state, widget.reportPathsType, widget.reportType, queries.previous),
       tabsReport: reportSelectors.selectReport(state, widget.reportPathsType, widget.reportType, queries.tabs),
       tabsReportFetchStatus: reportSelectors.selectReportFetchStatus(
@@ -58,6 +68,7 @@ const mapStateToProps = createMapStateToProps<DashboardWidgetOwnProps, Dashboard
 );
 
 const mapDispatchToProps: GcpDashboardWidgetDispatchProps = {
+  fetchForecasts: gcpDashboardActions.fetchWidgetForecasts,
   fetchReports: gcpDashboardActions.fetchWidgetReports,
   updateTab: gcpDashboardActions.changeWidgetTab,
 };
