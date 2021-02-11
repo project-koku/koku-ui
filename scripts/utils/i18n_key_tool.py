f9df70090fe08d9161a5e2fc9d8227007d49b82c
Doug Donahue <57504257+ddonahue007@users.noreply.github.com>
2021-02-11 16:17:41 -0500

    I18n dupes total (#1846)
    * removed unit.hrs i18n key (dead key)
    
    * always sort dict so report output is in same order
    
    * added 'totals' to duplicates tooling
    
    * Revert "removed unit.hrs i18n key (dead key)"
    
    This reverts commit 07e5b8ee
    
    * added ket back to excludes

--- scripts/utils/i18n_key_tool.py
+++ scripts/utils/i18n_key_tool.py
@@ -87,7 +87,7 @@ def find_duplicate_values(data):
 
     print(Colors.OKBLUE + 'Checking for duplicate key values...' + Colors.ENDC)
 
-    for k, v in data.items():
+    for k, v in sorted (data.items()):
         if v not in val_checked:
             res = []
 
@@ -97,7 +97,9 @@ def find_duplicate_values(data):
 
             if len(res) > 1:
                 total_dupes += 1
-                print(Colors.OKCYAN + '"' + v + '"' + Colors.ENDC)
+#                 print(Colors.OKCYAN + '"' + v + '"' + Colors.ENDC)
+                print('{:<80s}{:>10s}'.format(Colors.OKCYAN + '"' + v + '"', Colors.WARN + '[Found: ' + str(len(res)) +
+                 ']') + Colors.ENDC)
                 for dupe in res:
                     print(Colors.OKBLUE + "\tFOUND IN KEY: " + Colors.OKCYAN + dupe + Colors.ENDC)
 
