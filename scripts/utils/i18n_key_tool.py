e4294511920dfd00730dc157bda99bab5a6b8efc
Doug Donahue <57504257+ddonahue007@users.noreply.github.com>
2021-02-11 08:41:16 -0500

    initial tooling to check for duplicate values in i18n keys (#1844)
    * initial tooling to check for duplicate values in i18n keys
    
    * log message change

--- scripts/utils/i18n_key_tool.py
+++ scripts/utils/i18n_key_tool.py
@@ -8,14 +8,16 @@ import os
 not_found_cnt = 0
 found_cnt = 0
 exclude_cnt = 0
+key_vals = {}
+
 
 def check_dir(dir_name):
-    dir = os.path.join(os.getcwd(), dir_name)
+    directory = os.path.join(os.getcwd(), dir_name)
 
-    if os.path.isdir(dir):
-        return dir
+    if os.path.isdir(directory):
+        return directory
     else:
-        raise NotADirectoryError(dir)
+        raise NotADirectoryError(directory)
 
 
 def check_file(filename):
@@ -32,6 +34,8 @@ parser.add_argument('--search-path', type=check_dir, required=True, help='parent
 parser.add_argument('--json-file', type=check_file, required=True, help='i18n json file location')
 parser.add_argument('--exclude-file', type=check_file, help='file that lists i18n tags in dot notation to exclude'
                                                             'from reporting')
+parser.add_argument('--find-duplicates', action='store_true', help='report all duplicate values')
+
 parser.add_argument('--Xreport-found', action='store_false', help='do not report any "Found" keys')
 parser.add_argument('--Xreport-not-found', action='store_false', help='do not report any "Not Found" keys')
 args = parser.parse_args()
@@ -51,11 +55,19 @@ def walk_keys(obj, path=""):
     if isinstance(obj, dict):
         for k, v in obj.items():
             for r in walk_keys(v, path + "." + k if path else k):
+                # save the leaf key value pairs
+                if args.find_duplicates and not isinstance(v, (list, dict)):
+                    if not r.endswith("."):
+                        key_vals.update({r: v})
                 yield r
     elif isinstance(obj, list):
         for i, v in enumerate(obj):
             s = ""
             for r in walk_keys(v, path if path else s):
+                # save the leaf key value pairs
+                if args.find_duplicates and not isinstance(v, (list, dict)):
+                    if not r.endswith("."):
+                        key_vals.update({r: v})
                 yield r
     else:
         yield path
@@ -73,61 +85,100 @@ def search_for_key(path, key):
     return results
 
 
+# find duplicate values from dictionary using set
+def find_duplicate_values(data):
+    val_checked = []
+    total_dupes = 0
+
+    print(Colors.OKBLUE + 'Checking for duplicate key values...' + Colors.ENDC)
+
+    for k, v in data.items():
+        if v not in val_checked:
+            res = []
+
+            for key, value in data.items():
+                if v == value and key != k:
+                    res.append(key)
+
+            if len(res) > 1:
+                total_dupes += 1
+                print(Colors.OKCYAN + '"' + v + '"' + Colors.ENDC)
+                for dupe in res:
+                    print(Colors.OKBLUE + "\tFOUND IN KEY: " + Colors.FAIL + dupe + Colors.ENDC)
+
+            val_checked.append(v)
+
+    print("\n")
+    print(Colors.OKCYAN + "TOTAL DUPLICATES FOUND: ", Colors.OKBLUE + str(total_dupes) + Colors.ENDC)
+
+
 json_data = json.load(open(args.json_file))
 previous_key_status = {}
 
-for i18n_key in sorted(list(set(walk_keys(json_data)))):
-    exclude_data = []
-    found = False
+# Check all i18n_keys to see if they are being used in the src code
+if args.Xreport_not_found or args.Xreport_found:
+    print(Colors.OKBLUE + 'Checking for dead keys...' + Colors.ENDC)
+    for i18n_key in sorted(list(set(walk_keys(json_data)))):
+        exclude_data = []
+        found = False
 
-    # check if excludelist is given
-    if args.exclude_file is not None:
-        with open(args.exclude_file) as f:
-            exclude_data = f.read().splitlines()
+        # check if exclude list is given
+        if args.exclude_file is not None:
+            with open(args.exclude_file) as f:
+                exclude_data = f.read().splitlines()
 
-    if exclude_data.__contains__(i18n_key):
-        # key is on exclude list
-        previous_key_status.clear()
-        previous_key_status[i18n_key] = True
-        print('{:<80s}{:>10s}'.format(Colors.OKCYAN + i18n_key, Colors.WARN + '[TAG EXCLUDED]') + Colors.ENDC)
-        exclude_cnt += 1
-    else:
-        result = search_for_key(args.search_path, i18n_key)
-
-        # check plurals and exclude if none plural is already found
-        if len(previous_key_status) > 0:
-            prev_key = next(iter(previous_key_status))
-            if prev_key + "_plural" == i18n_key and previous_key_status[prev_key]:
-                previous_key_status.clear()
-                print('{:<80s}{:>10s}'.format(Colors.OKCYAN + i18n_key, Colors.WARN + '[TAG EXCLUDED]') + Colors.ENDC)
-                exclude_cnt += 1
-                continue
-
-        # found status
-        if len(result) <= 0:
-            found = False
-            previous_key_status.clear()
-            previous_key_status[i18n_key] = False
-            not_found_cnt += 1
-        else:
-            found = True
+        if exclude_data.__contains__(i18n_key):
+            # key is on exclude list
             previous_key_status.clear()
             previous_key_status[i18n_key] = True
-            found_cnt += 1
+            print('{:<80s}{:>10s}'.format(Colors.OKCYAN + i18n_key, Colors.WARN + '[TAG EXCLUDED]') + Colors.ENDC)
+            exclude_cnt += 1
+        else:
+            result = search_for_key(args.search_path, i18n_key)
 
-        # report based on cli args
-        if found is False and args.Xreport_not_found:
-            print('{:<80s}{:>10s}'.format(Colors.OKCYAN + i18n_key, Colors.FAIL + '[NOT FOUND]') + Colors.ENDC)
-            continue
+            # check plurals and exclude if none plural is already found
+            if len(previous_key_status) > 0:
+                prev_key = next(iter(previous_key_status))
+                if prev_key + "_plural" == i18n_key and previous_key_status[prev_key]:
+                    previous_key_status.clear()
+                    print('{:<80s}{:>10s}'.format(Colors.OKCYAN + i18n_key, Colors.WARN + '[TAG EXCLUDED]') + Colors.ENDC)
+                    exclude_cnt += 1
+                    continue
 
-        if args.Xreport_found:
-            print('{:<80s}{:>10s}'.format(Colors.OKCYAN + i18n_key, Colors.PASS + '[FOUND]') + Colors.ENDC)
-            for f in result:
-                print(Colors.OKBLUE + "\tFOUND IN: " + f + Colors.ENDC)
+            # found status
+            if len(result) <= 0:
+                found = False
+                previous_key_status.clear()
+                previous_key_status[i18n_key] = False
+                not_found_cnt += 1
+            else:
+                found = True
+                previous_key_status.clear()
+                previous_key_status[i18n_key] = True
+                found_cnt += 1
 
-# totals
-print("\n")
-print('{:>25s}'.format(Colors.OKCYAN + "TOTALS" + Colors.ENDC))
-print('{:<30s}{:>10s}'.format(Colors.OKCYAN + "FOUND", Colors.OKBLUE + str(found_cnt) + Colors.ENDC))
-print('{:<30s}{:>10s}'.format(Colors.OKCYAN + "NOT FOUND", Colors.FAIL + str(not_found_cnt) + Colors.ENDC))
-print('{:<30s}{:>10s}'.format(Colors.OKCYAN + "EXCLUDED", Colors.WARN + str(exclude_cnt) + Colors.ENDC))
+            # report based on cli args
+            if found is False and args.Xreport_not_found:
+                print('{:<80s}{:>10s}'.format(Colors.OKCYAN + i18n_key, Colors.FAIL + '[NOT FOUND]') + Colors.ENDC)
+                continue
+
+            if args.Xreport_found:
+                print('{:<80s}{:>10s}'.format(Colors.OKCYAN + i18n_key, Colors.PASS + '[FOUND]') + Colors.ENDC)
+                for f in result:
+                    print(Colors.OKBLUE + "\tFOUND IN: " + f + Colors.ENDC)
+
+    # Dead Key Report Totals
+    print("\n")
+    print('{:>30s}'.format(Colors.OKCYAN + "DEAD KEY TOTALS" + Colors.ENDC))
+    print('{:<30s}{:>10s}'.format(Colors.OKCYAN + "FOUND", Colors.OKBLUE + str(found_cnt) + Colors.ENDC))
+    print('{:<30s}{:>10s}'.format(Colors.OKCYAN + "NOT FOUND", Colors.FAIL + str(not_found_cnt) + Colors.ENDC))
+    print('{:<30s}{:>10s}'.format(Colors.OKCYAN + "EXCLUDED", Colors.WARN + str(exclude_cnt) + Colors.ENDC))
+    print('{:<30s}{:>10s}'.format(Colors.OKCYAN + "TOTAL NUM OF KEYS", Colors.OKBLUE + str(found_cnt + not_found_cnt +
+                                                                                           exclude_cnt) + Colors.ENDC))
+
+# Report all duplicate values in i18n file
+if args.find_duplicates:
+    print("\n")
+    for i18n_key in sorted(list(set(walk_keys(json_data)))):
+        pass
+    find_duplicate_values(key_vals)
