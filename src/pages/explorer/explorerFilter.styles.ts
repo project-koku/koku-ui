95849d86744b337d838e3e56c14ab4eba090aecb
Dan Labrecque <dlabrecq@redhat.com>
2021-02-12 11:02:59 -0500

    Adjust explorer filter padding

--- src/pages/explorer/explorerFilter.styles.ts
+++ src/pages/explorer/explorerFilter.styles.ts
@@ -1,11 +1,12 @@
 import global_BackgroundColor_light_100 from '@patternfly/react-tokens/dist/js/global_BackgroundColor_light_100';
 import global_spacer_md from '@patternfly/react-tokens/dist/js/global_spacer_md';
+import global_spacer_sm from '@patternfly/react-tokens/dist/js/global_spacer_sm';
 import React from 'react';
 
 export const styles = {
   toolbarContainer: {
     backgroundColor: global_BackgroundColor_light_100.value,
     marginLeft: `-${global_spacer_md.value}`,
-    paddingTop: global_spacer_md.value,
+    paddingTop: global_spacer_sm.value,
   },
 } as { [className: string]: React.CSSProperties };
