067206037045ab37a03a309b19281027ffcaa07b
Dan Labrecque <dlabrecq@redhat.com>
2021-02-15 14:11:50 -0500

    updated frontend-components-utilities import path

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-import { authInterceptor as insightsAuthInterceptor } from '@redhat-cloud-services/frontend-components-utilities/esm/interceptors';
+import { authInterceptor as insightsAuthInterceptor } from '@redhat-cloud-services/frontend-components-utilities/interceptors';
 import axios, { AxiosRequestConfig } from 'axios';
 
 export interface PagedMetaData {
