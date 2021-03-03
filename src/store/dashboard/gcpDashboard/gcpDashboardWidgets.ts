98c6caa41db4d1a34d8c269b9d23052f88808c0a
Doug Donahue <57504257+ddonahue007@users.noreply.github.com>
2021-03-03 09:24:09 -0500

    fixes for i18n keys, using old key name (#1892)
    added keys to i18n exludelist
    removed dead keys

--- src/store/dashboard/gcpDashboard/gcpDashboardWidgets.ts
+++ src/store/dashboard/gcpDashboard/gcpDashboardWidgets.ts
@@ -119,7 +119,7 @@ export const databaseWidget: GcpDashboardWidget = {
     computedReportItem: ComputedReportItemType.cost,
     computedReportItemValue: ComputedReportItemValueType.total,
     formatOptions: {},
-    titleKey: 'chart.cumulative_cost_comparison',
+    titleKey: 'dashboard.cumulative_cost_comparison',
     type: ChartType.rolling,
   },
   topItems: {
@@ -158,7 +158,7 @@ export const networkWidget: GcpDashboardWidget = {
     computedReportItem: ComputedReportItemType.cost,
     computedReportItemValue: ComputedReportItemValueType.total,
     formatOptions: {},
-    titleKey: 'chart.cumulative_cost_comparison',
+    titleKey: 'dashboard.cumulative_cost_comparison',
     type: ChartType.rolling,
   },
   topItems: {
