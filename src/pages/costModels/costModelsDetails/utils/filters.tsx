41fdd792119408bfefae20850f07feb773fab199
Doug Donahue <57504257+ddonahue007@users.noreply.github.com>
2021-04-14 09:35:12 -0400

    fix for cost model filter search input (#1944)
    * fix for cost model filter search input
    
    * removed commented out code
    
    * removed id prop from search interface. This was causing cached history values to fill the filter input on select.

--- src/pages/costModels/costModelsDetails/utils/filters.tsx
+++ src/pages/costModels/costModelsDetails/utils/filters.tsx
@@ -2,12 +2,15 @@ import {
   Dropdown,
   DropdownItem,
   DropdownToggle,
-  SearchInput,
+  InputGroup,
+  InputGroupText,
+  TextInput,
   Toolbar,
   ToolbarFilter,
   ToolbarFilterProps,
   ToolbarProps,
 } from '@patternfly/react-core';
+import { SearchIcon } from '@patternfly/react-icons/dist/js/icons/search-icon';
 import HookIntoProps from 'hook-into-props';
 import React from 'react';
 import { WithTranslation, withTranslation } from 'react-i18next';
@@ -18,6 +21,34 @@ import { costModelsSelectors } from 'store/costModels';
 
 import { CostModelsQuery, initialCostModelsQuery, stringifySearch } from './query';
 import { HistoryPush, Inputer, Opener } from './types';
+
+interface SearchInputProps {
+  value: string;
+  onChange: (value: string, event: React.FormEvent<HTMLInputElement>) => void;
+  onKeyPress: (evt: React.KeyboardEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+}
+
+const SearchInput: React.SFC<SearchInputProps> = ({ placeholder = '', value, onChange, onKeyPress }) => {
+  return (
+    <InputGroup>
+      <TextInput
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+        onKeyPress={(evt: React.KeyboardEvent<HTMLInputElement>) => {
+          if (evt.key !== 'Enter' || value === '') {
+            return;
+          }
+          onKeyPress(evt);
+        }}
+      />
+      <InputGroupText style={{ borderLeft: '0' }}>
+        <SearchIcon />
+      </InputGroupText>
+    </InputGroup>
+  );
+};
 
 export const onKeyPress = (push: HistoryPush, key: string, query: CostModelsQuery, inputer: Inputer) => (
   event: React.KeyboardEvent<HTMLInputElement>
@@ -73,14 +104,9 @@ const descriptionMergeProps = (
   const children =
     filterType === 'description' ? (
       <SearchInput
-        onClear={() => {
-          setValue('');
-        }}
         placeholder={t('page_cost_models.filter_by_description')}
         value={value}
-        onChange={(text: string) => {
-          setValue(text);
-        }}
+        onChange={(text: string) => setValue(text)}
         onKeyPress={onKeyPress(push, 'description', { ...initialCostModelsQuery, ...query }, { value, setValue })}
       />
     ) : null;
@@ -121,11 +147,10 @@ const nameFilterMergeProps = (
   const children =
     filterType === 'name' ? (
       <SearchInput
-        onClear={() => setValue('')}
         placeholder={t('page_cost_models.filter_by_name')}
         value={value}
         onChange={(text: string) => setValue(text)}
-        onKeyPress={onKeyPress(push, 'name', { ...initialCostModelsQuery, ...query }, { setValue, value })}
+        onKeyPress={onKeyPress(push, 'name', { ...initialCostModelsQuery, ...query }, { value, setValue })}
       />
     ) : null;
   const chips = query.name ? query.name.split(',') : [];
