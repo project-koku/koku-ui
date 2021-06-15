259653fd4942b0ab18650a10c1456a69ba4e8de9
Dan Labrecque <dlabrecq@redhat.com>
2021-06-14 22:31:41 -0400

    Added API support for resource-types API
    https://issues.redhat.com/browse/COST-1494

---
+++ src/api/resources/resource.ts
@@ -0,0 +1,32 @@
+export interface ResourceData {
+  value?: string;
+}
+
+export interface ResourceMeta {
+  count: number;
+}
+
+export interface ResourceLinks {
+  first: string;
+  previous?: string;
+  next?: string;
+  last: string;
+}
+
+export interface Resource {
+  meta: ResourceMeta;
+  links: ResourceLinks;
+  data: ResourceData[];
+}
+
+// eslint-disable-next-line no-shadow
+export const enum ResourceType {
+  account = 'account',
+  region = 'region',
+  service = 'service',
+}
+
+// eslint-disable-next-line no-shadow
+export const enum ResourcePathsType {
+  aws = 'aws',
+}
---
+++ src/api/resources/resourceUtils.ts
@@ -0,0 +1,27 @@
+import { runResource as runAwsResource } from './awsResource';
+import { ResourcePathsType, ResourceType } from './resource';
+
+export function isResourceTypeValid(resourcePathsType: ResourcePathsType, resourceType: ResourceType) {
+  let result = false;
+
+  if (resourcePathsType === ResourcePathsType.aws) {
+    switch (resourceType) {
+      case ResourceType.account:
+      case ResourceType.region:
+      case ResourceType.service:
+        result = true;
+        break;
+    }
+  }
+  return result;
+};
+
+export function runResource(resourcePathsType: ResourcePathsType, resourceType: ResourceType, query: string) {
+  let forecast;
+  switch (resourcePathsType) {
+    case ResourcePathsType.aws:
+      forecast = runAwsResource(resourceType, query);
+      break;
+  }
+  return forecast;
+}
