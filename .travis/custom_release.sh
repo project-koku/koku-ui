913f8954be238bc15efade78f18ea308b2b3740b
Dan Labrecque <dlabrecq@redhat.com>
2021-07-22 17:18:41 -0400

    Custom release script for stage-beta
    https://issues.redhat.com/browse/COST-1715

---
+++ .travis/custom_release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+# If current dev branch is master, push to build repo stage-beta
+if [[ "${TRAVIS_BRANCH}" = "master" || "${TRAVIS_BRANCH}" = "main" ]]; then
+  .travis/release.sh "stage-beta"
+fi
+
+# If current dev branch is deployment branch, push to build repo
+if [[ "${TRAVIS_BRANCH}" = "stage-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
+  .travis/release.sh "${TRAVIS_BRANCH}"
+fi
