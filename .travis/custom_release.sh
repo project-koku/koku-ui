4073cb1341e6ecc9946f3bd6bce7a4b16bcb2140
Dan Labrecque <dlabrecq@redhat.com>
2021-07-26 09:46:05 -0400

    Added ci-beta and ci-stable to custom release script

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -4,10 +4,11 @@ set -x
 
 # If current dev branch is master, push to build repo stage-beta
 if [[ "${TRAVIS_BRANCH}" = "master" || "${TRAVIS_BRANCH}" = "main" ]]; then
+  .travis/release.sh "ci-beta" ## Continue to push ci-beta for now
   .travis/release.sh "stage-beta"
 fi
 
 # If current dev branch is deployment branch, push to build repo
-if [[ "${TRAVIS_BRANCH}" = "stage-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
+if [[ "${TRAVIS_BRANCH}" = "ci-stable" || "${TRAVIS_BRANCH}" = "stage-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
   .travis/release.sh "${TRAVIS_BRANCH}"
 fi
