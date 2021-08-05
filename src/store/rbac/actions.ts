bb52433fc11d24c13788b970c58012399019efb1
Doug Donahue <57504257+ddonahue007@users.noreply.github.com>
2021-08-05 18:05:50 -0400

    React intl chg ovr (#2065)
    * Change over for action classes
    
    * revert manifest updates

--- src/store/rbac/actions.ts
+++ src/store/rbac/actions.ts
@@ -1,6 +1,7 @@
 import { addNotification } from '@redhat-cloud-services/frontend-components-notifications';
 import { getRBAC, RBAC } from 'api/rbac';
-import i18next from 'i18next';
+import { createIntlEnv } from 'components/i18n/localeEnv';
+import messages from 'locales/messages';
 import { Dispatch } from 'redux';
 import { createAsyncAction } from 'typesafe-actions';
 
@@ -11,6 +12,7 @@ export const {
 } = createAsyncAction('fetch/RBAC/request', 'fetch/RBAC/success', 'fetch/RBAC/failure')<void, RBAC, Error>();
 
 export const fetchRbac = (): any => {
+  const intl = createIntlEnv();
   return (dispatch: Dispatch) => {
     dispatch(fetchRbacRequest());
     return getRBAC()
@@ -20,8 +22,8 @@ export const fetchRbac = (): any => {
       .catch(err => {
         dispatch(
           addNotification({
-            title: i18next.t('rbac.error_title'),
-            description: i18next.t('rbac.error_description'),
+            title: intl.formatMessage(messages.RbacErrorTitle),
+            description: intl.formatMessage(messages.RbacErrorDescription),
             variant: 'danger',
             dismissable: true,
           })
