49049984943a2fb7234b3c6cec5bde19b90263e3
Doug Donahue <ddonahue@redhat.com>
2021-08-10 21:01:01 -0400

    Removed i18Next from overview dashbords

--- src/pages/views/overview/azureDashboard/azureDashboard.tsx
+++ src/pages/views/overview/azureDashboard/azureDashboard.tsx
@@ -1,12 +1,11 @@
 import { DashboardBase } from 'pages/views/overview/components/dashboardBase';
-import { WithTranslation, withTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 import { createMapStateToProps } from 'store/common';
 import { azureDashboardSelectors } from 'store/dashboard/azureDashboard';
 
 import { AzureDashboardWidget } from './azureDashboardWidget';
 
-type AzureDashboardOwnProps = WithTranslation;
+type AzureDashboardOwnProps = any;
 
 interface AzureDashboardStateProps {
   DashboardWidget: typeof AzureDashboardWidget;
@@ -22,6 +21,6 @@ const mapStateToProps = createMapStateToProps<AzureDashboardOwnProps, AzureDashb
   };
 });
 
-const AzureDashboard = withTranslation()(connect(mapStateToProps, {})(DashboardBase));
+const AzureDashboard = connect(mapStateToProps, {})(DashboardBase);
 
 export default AzureDashboard;
