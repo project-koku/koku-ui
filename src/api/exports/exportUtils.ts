86f94b47711d7037999559e449f3867b42ead1e4
Dan Labrecque <dlabrecq@redhat.com>
2021-08-10 23:14:58 -0400

    Added export support for "filtered by OpenShift" perspectives

--- src/api/exports/exportUtils.ts
+++ src/api/exports/exportUtils.ts
@@ -1,8 +1,11 @@
 import { ReportPathsType, ReportType } from 'api/reports/report';
 
 import { runExport as runAwsExport } from './awsExport';
+import { runExport as runAwsOcpExport } from './awsOcpExport';
 import { runExport as runAzureExport } from './azureExport';
+import { runExport as runAzureOcpExport } from './azureOcpExport';
 import { runExport as runGcpExport } from './gcpExport';
+import { runExport as runGcpOcpExport } from './gcpOcpExport';
 import { runExport as runIbmExport } from './ibmExport';
 import { runExport as runOcpCloudExport } from './ocpCloudExport';
 import { runExport as runOcpExport } from './ocpExport';
@@ -13,11 +16,20 @@ export function runExport(reportPathsType: ReportPathsType, reportType: ReportTy
     case ReportPathsType.aws:
       report = runAwsExport(reportType, query);
       break;
+    case ReportPathsType.awsOcp:
+      report = runAwsOcpExport(reportType, query);
+      break;
     case ReportPathsType.azure:
       report = runAzureExport(reportType, query);
       break;
+    case ReportPathsType.azureOcp:
+      report = runAzureOcpExport(reportType, query);
+      break;
     case ReportPathsType.gcp:
       report = runGcpExport(reportType, query);
+      break;
+    case ReportPathsType.gcpOcp:
+      report = runGcpOcpExport(reportType, query);
       break;
     case ReportPathsType.ibm:
       report = runIbmExport(reportType, query);
