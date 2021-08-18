1db87451e116895a6cdcc7b3bb856c5f8564be2e
Dan Labrecque <dlabrecq@redhat.com>
2021-08-18 14:42:33 -0400

    Added react-intl mocks and fixed tests

--- src/__mocks__/react-i18next.ts
+++ src/__mocks__/react-i18next.ts
@@ -1,9 +1,15 @@
 const mockedReactI18next = jest.genMockFromModule('react-i18next') as any;
 
 const t = jest.fn(v => v);
+const i18n = {
+  changeLanguage: () => new Promise(() => {}),
+};
 
 mockedReactI18next.I18n = jest.fn(({ children }) => children(t));
-mockedReactI18next.withTranslation = jest.fn(() => jest.fn(v => v));
-mockedReactI18next.useTranslation = jest.fn(() => ({ t: jest.fn(v => v) }));
+mockedReactI18next.withTranslation = () => Component => {
+  Component.defaultProps = { ...Component.defaultProps, t: (v: string) => v };
+  return Component;
+};
+mockedReactI18next.useTranslation = jest.fn(() => ({ t, i18n }));
 
 module.exports = mockedReactI18next;
