fac98c4275c2188f5655ff5058cd39483f6a0669
Dan Labrecque <dlabrecq@redhat.com>
2021-08-27 12:12:12 -0400

    Cost abbreviations (#2131)
    * Adjusted the cost pie chart width
    
    * Fixed cost abbreviations for cost explorer chart

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -80,11 +80,11 @@ export const formatCurrencyAbbreviation: ValueFormatter = (value, unit, { fracti
 
   // Derived from https://stackoverflow.com/questions/37799955/how-can-i-format-big-numbers-with-tolocalestring
   const abbreviationFormats = [
-    { val: 1e15, symbol: 'currency_abbreviations.quadrillion' },
-    { val: 1e12, symbol: 'currency_abbreviations.trillion' },
-    { val: 1e9, symbol: 'currency_abbreviations.billion' },
-    { val: 1e6, symbol: 'currency_abbreviations.million' },
-    { val: 1e3, symbol: 'currency_abbreviations.thousand' },
+    { val: 1e15, symbol: 'quadrillion' },
+    { val: 1e12, symbol: 'trillion' },
+    { val: 1e9, symbol: 'billion' },
+    { val: 1e6, symbol: 'million' },
+    { val: 1e3, symbol: 'thousand' },
   ];
 
   // Find the proper format to use
@@ -103,7 +103,8 @@ export const formatCurrencyAbbreviation: ValueFormatter = (value, unit, { fracti
       maximumFractionDigits: fractionDigits,
     });
     const parts = formatted.match(/([\D]*)([\d.,]+)([\D]*)/);
-    return `${parts[1]}${parts[2]}${intlHelper(intl.formatMessage(messages.Custom, { msg: symbol }))}${parts[3]}`;
+    const abbr = intlHelper(intl.formatMessage(messages.CurrencyAbbreviations, { value: symbol }));
+    return `${parts[1]}${parts[2]}${abbr}${parts[3]}`;
   }
 
   // If no format was found, format value without abbreviation
