901a3f219803af94556c190249e412ea9967d3d5
Doug Donahue <57504257+ddonahue007@users.noreply.github.com>
2021-09-01 18:53:29 -0400

    React intl create costmodel (#2167)
    * create cost model
    
    * markup initial react-intl changes (messages)
    
    * more stuff
    
    * react-intl  create cost model wizard
    
    * fixed price list
    
    * skip test for now
    
    * fixed description and title on create cost model wizard

--- src/pages/costModels/createCostModelWizard/steps.tsx
+++ src/pages/costModels/createCostModelWizard/steps.tsx
@@ -1,130 +1,22 @@
-import React from 'react';
+import messages from 'locales/messages';
+import { MessageDescriptor } from 'react-intl';
 
-import GeneralInformation from './generalInformation';
-import Markup from './markup';
-import PriceList from './priceList';
-import Review from './review';
-import Sources from './sources';
-
-export const nameErrors = (name: string): string | null => {
+export const nameErrors = (name: string): MessageDescriptor | null => {
   if (name.length === 0) {
-    return 'cost_models.required_field';
+    return messages.CostModelsRequiredField;
   }
   if (name.length > 100) {
-    return 'cost_models_wizard.general_info.name_too_long';
+    return messages.CostModelsInfoTooLong;
   }
   return null;
 };
 
-export const descriptionErrors = (description: string): string | null => {
+export const descriptionErrors = (description: string): MessageDescriptor | null => {
   if (description.length > 500) {
-    return 'cost_models.description_too_long';
+    return messages.CostModelsDescTooLong;
   }
   return null;
 };
-
-export const stepsHash = (t: (text: string) => string) => ({
-  '': [
-    {
-      id: 1,
-      name: t('cost_models_wizard.steps.general_info'),
-      component: <GeneralInformation />,
-    },
-  ],
-  AZURE: [
-    {
-      id: 1,
-      name: t('cost_models_wizard.steps.general_info'),
-      component: <GeneralInformation />,
-    },
-    {
-      id: 2,
-      name: t('cost_calculations'),
-      component: <Markup />,
-    },
-    {
-      id: 3,
-      name: t('cost_models_wizard.steps.sources'),
-      component: <Sources />,
-    },
-    {
-      id: 4,
-      name: t('cost_models_wizard.steps.review'),
-      component: <Review />,
-    },
-  ],
-  AWS: [
-    {
-      id: 1,
-      name: t('cost_models_wizard.steps.general_info'),
-      component: <GeneralInformation />,
-    },
-    {
-      id: 2,
-      name: t('cost_calculations'),
-      component: <Markup />,
-    },
-    {
-      id: 3,
-      name: t('cost_models_wizard.steps.sources'),
-      component: <Sources />,
-    },
-    {
-      id: 4,
-      name: t('cost_models_wizard.steps.review'),
-      component: <Review />,
-    },
-  ],
-  GCP: [
-    {
-      id: 1,
-      name: t('cost_models_wizard.steps.general_info'),
-      component: <GeneralInformation />,
-    },
-    {
-      id: 2,
-      name: t('cost_calculations'),
-      component: <Markup />,
-    },
-    {
-      id: 3,
-      name: t('cost_models_wizard.steps.sources'),
-      component: <Sources />,
-    },
-    {
-      id: 4,
-      name: t('cost_models_wizard.steps.review'),
-      component: <Review />,
-    },
-  ],
-  OCP: [
-    {
-      id: 1,
-      name: t('cost_models_wizard.steps.general_info'),
-      component: <GeneralInformation />,
-    },
-    {
-      id: 2,
-      name: t('cost_models_wizard.steps.price_list'),
-      component: <PriceList />,
-    },
-    {
-      id: 3,
-      name: t('cost_calculations'),
-      component: <Markup />,
-    },
-    {
-      id: 4,
-      name: t('cost_models_wizard.steps.sources'),
-      component: <Sources />,
-    },
-    {
-      id: 5,
-      name: t('cost_models_wizard.steps.review'),
-      component: <Review />,
-    },
-  ],
-});
 
 export const validatorsHash = {
   '': [() => false],
