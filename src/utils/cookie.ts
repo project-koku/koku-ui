c2a94f753fdf59a538d3e6e87e9d84b71095beda
Dan Labrecque <dlabrecq@redhat.com>
2021-09-10 01:36:06 -0400

    Fixed lint issues

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -5,19 +5,19 @@ export const deleteSessionCookie = name => {
     now.setTime(now.getTime() - 3600);
     document.cookie = `${name}=; expires=${now.toUTCString()}; path=/`;
   }
-}
+};
 
 export const getCookie = name => {
   const cookie = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
   return cookie ? cookie.pop() : '';
-}
+};
 
 // Returns the last 40 chars of the session token
 export const getTokenCookie = () => {
   const token = getCookie('cs_jwt');
   return token.substring(token.length - 40, token.length);
-}
+};
 
 export const setSessionCookie = (name, value) => {
   document.cookie = `${name}=${value}; path=/`;
-}
+};
