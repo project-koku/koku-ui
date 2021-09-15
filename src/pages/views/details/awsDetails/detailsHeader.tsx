3f20df94cbace45edfd1e28355861e9b4fb769c2
Dan Labrecque <dlabrecq@redhat.com>
2021-09-15 12:49:56 -0400

    Added currency layout to AWS details header

--- src/pages/views/details/awsDetails/detailsHeader.tsx
+++ src/pages/views/details/awsDetails/detailsHeader.tsx
@@ -6,6 +6,7 @@ import { getProvidersQuery } from 'api/queries/providersQuery';
 import { AwsReport } from 'api/reports/awsReports';
 import { TagPathsType } from 'api/tags/tag';
 import { AxiosError } from 'axios';
+import { Currency } from 'components/currency/currency';
 import messages from 'locales/messages';
 import { GroupBy } from 'pages/views/components/groupBy/groupBy';
 import React from 'react';
@@ -65,10 +66,13 @@ class DetailsHeaderBase extends React.Component<DetailsHeaderProps> {
 
     return (
       <header style={styles.header}>
-        <div>
+        <div style={styles.headerContent}>
           <Title headingLevel="h1" style={styles.title} size={TitleSizes['2xl']}>
             {intl.formatMessage(messages.AWSDetailsTitle)}
           </Title>
+          <Currency />
+        </div>
+        <div style={styles.headerContent}>
           <GroupBy
             getIdKeyForGroupBy={getIdKeyForGroupBy}
             groupBy={groupBy}
@@ -80,18 +84,18 @@ class DetailsHeaderBase extends React.Component<DetailsHeaderProps> {
             showTags
             tagReportPathsType={tagReportPathsType}
           />
+          {Boolean(showContent) && (
+            <div>
+              <Title headingLevel="h2" style={styles.costValue} size={TitleSizes['4xl']}>
+                {formatCurrency(
+                  hasCost ? report.meta.total.cost.total.value : 0,
+                  hasCost ? report.meta.total.cost.total.units : 'USD'
+                )}
+              </Title>
+              <div style={styles.dateTitle}>{getSinceDateRangeString()}</div>
+            </div>
+          )}
         </div>
-        {Boolean(showContent) && (
-          <div>
-            <Title headingLevel="h2" style={styles.costValue} size={TitleSizes['4xl']}>
-              {formatCurrency(
-                hasCost ? report.meta.total.cost.total.value : 0,
-                hasCost ? report.meta.total.cost.total.units : 'USD'
-              )}
-            </Title>
-            <div style={styles.dateTitle}>{getSinceDateRangeString()}</div>
-          </div>
-        )}
       </header>
     );
   }
