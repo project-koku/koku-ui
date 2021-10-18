9ae3a870109b50a1ab3e1d0137b8b34a1f9bb6cd
Dan Labrecque <dlabrecq@redhat.com>
2021-10-18 09:20:53 -0400

    Standalone mode hard codes isBeta flag

--- scripts/start-dev-server.js
+++ scripts/start-dev-server.js
@@ -33,7 +33,6 @@ async function setEnv() {
       process.env.USE_LOCAL_ROUTES = localApi.toString();
       if (localApi) {
         process.env.USE_PROXY = 'false';
-        process.env.BETA_ENV = 'true';
       }
     });
 }
