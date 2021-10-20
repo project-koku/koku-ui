0d817ec017d3717e9e7622f8adc8a55191d44f0a
Dan Labrecque <dlabrecq@redhat.com>
2021-10-20 13:07:59 -0400

    Fixed missing bracket in custom release script

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -8,6 +8,6 @@ if [[ "${TRAVIS_BRANCH}" = "master" || "${TRAVIS_BRANCH}" = "main" ]]; then
 fi
 
 # If current dev branch is deployment branch, push to build repo
-if [[ "${TRAVIS_BRANCH}" = "stage-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]; then
+if [[ "${TRAVIS_BRANCH}" = "stage-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
   .travis/release.sh "${TRAVIS_BRANCH}"
 fi
