fa7fba7d13ee387bb9fb1e7b75ad4aa87ef10a50
Dan Labrecque <dlabrecq@redhat.com>
2021-12-02 11:47:20 -0500

    Ensure cost type is fetched only if user has RBAC permissions

--- src/components/costType/costTypeWrapper.tsx
+++ src/components/costType/costTypeWrapper.tsx
@@ -1,11 +1,18 @@
 import { CostType } from 'api/costType';
+import { Providers, ProviderType } from 'api/providers';
+import { getProvidersQuery } from 'api/queries/providersQuery';
+import { getUserAccessQuery } from 'api/queries/userAccessQuery';
+import { UserAccess, UserAccessType } from 'api/userAccess';
 import { AxiosError } from 'axios';
 import React from 'react';
 import { injectIntl, WrappedComponentProps } from 'react-intl';
 import { connect } from 'react-redux';
 import { createMapStateToProps, FetchStatus } from 'store/common';
 import { costTypeActions, costTypeSelectors } from 'store/costType';
+import { awsProvidersQuery, providersSelectors } from 'store/providers';
+import { allUserAccessQuery, userAccessSelectors } from 'store/userAccess';
 import { CostTypes } from 'utils/localStorage';
+import { isAwsAvailable } from 'utils/userAccess';
 
 interface CostTypeWrapperOwnProps {
   children?: React.ReactNode;
@@ -16,9 +23,16 @@ interface CostTypeWrapperDispatchProps {
 }
 
 interface CostTypeWrapperStateProps {
+  awsProviders: Providers;
+  awsProvidersFetchStatus: FetchStatus;
+  awsProvidersQueryString: string;
   costType: CostType;
   costTypeError: AxiosError;
   costTypeFetchStatus?: FetchStatus;
+  userAccess: UserAccess;
+  userAccessError: AxiosError;
+  userAccessFetchStatus: FetchStatus;
+  userAccessQueryString: string;
 }
 
 interface CostTypeWrapperState {
@@ -37,17 +51,15 @@ class CostTypeWrapperBase extends React.Component<CostTypeWrapperProps> {
   public state: CostTypeWrapperState = { ...this.defaultState };
 
   public componentDidMount() {
-    const { costTypeFetchStatus, fetchCostType } = this.props;
-
-    if (costTypeFetchStatus !== FetchStatus.inProgress) {
-      fetchCostType();
-    }
+    this.updateCostType();
   }
 
   public componentDidUpdate(prevProps: CostTypeWrapperProps, prevState: CostTypeWrapperState) {
-    const { costType } = this.props;
+    const { awsProviders, costType } = this.props;
 
-    if (prevProps.costType !== costType) {
+    if (prevProps.awsProviders !== awsProviders) {
+      this.updateCostType();
+    } else if (prevProps.costType !== costType) {
       const currentCostType = costType ? costType.meta['cost-type'] : CostTypes.unblended;
 
       this.setState({
@@ -56,23 +68,63 @@ class CostTypeWrapperBase extends React.Component<CostTypeWrapperProps> {
     }
   }
 
+  private isAwsAvailable = () => {
+    const { awsProviders, awsProvidersFetchStatus, userAccess } = this.props;
+    return isAwsAvailable(userAccess, awsProviders, awsProvidersFetchStatus);
+  };
+
+  private updateCostType = () => {
+    const { costTypeFetchStatus, fetchCostType } = this.props;
+
+    if (this.isAwsAvailable() && costTypeFetchStatus !== FetchStatus.inProgress) {
+      fetchCostType();
+    }
+  };
+
   public render() {
-    const { children } = this.props;
+    const { awsProvidersFetchStatus, children } = this.props;
     const { currentCostType } = this.state;
 
+    if (!this.isAwsAvailable() && awsProvidersFetchStatus === FetchStatus.complete) {
+      return children;
+    }
     return currentCostType ? children : null;
   }
 }
 
 const mapStateToProps = createMapStateToProps<CostTypeWrapperOwnProps, CostTypeWrapperStateProps>(state => {
+  const awsProvidersQueryString = getProvidersQuery(awsProvidersQuery);
+  const awsProviders = providersSelectors.selectProviders(state, ProviderType.aws, awsProvidersQueryString);
+  const awsProvidersFetchStatus = providersSelectors.selectProvidersFetchStatus(
+    state,
+    ProviderType.aws,
+    awsProvidersQueryString
+  );
+
   const costType = costTypeSelectors.selectCostType(state);
   const costTypeError = costTypeSelectors.selectCostTypeError(state);
   const costTypeFetchStatus = costTypeSelectors.selectCostTypeFetchStatus(state);
 
+  const userAccessQueryString = getUserAccessQuery(allUserAccessQuery);
+  const userAccess = userAccessSelectors.selectUserAccess(state, UserAccessType.all, userAccessQueryString);
+  const userAccessError = userAccessSelectors.selectUserAccessError(state, UserAccessType.all, userAccessQueryString);
+  const userAccessFetchStatus = userAccessSelectors.selectUserAccessFetchStatus(
+    state,
+    UserAccessType.all,
+    userAccessQueryString
+  );
+
   return {
+    awsProviders,
+    awsProvidersFetchStatus,
+    awsProvidersQueryString,
     costType,
     costTypeError,
     costTypeFetchStatus,
+    userAccess,
+    userAccessError,
+    userAccessFetchStatus,
+    userAccessQueryString,
   };
 });
 
