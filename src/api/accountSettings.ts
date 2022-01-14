700d41908e51b2ba7aac25961fd58a28293275f1
Dan Labrecque <dlabrecq@redhat.com>
2022-01-14 12:48:05 -0500

    Replace cost-type with account-settings API
    https://issues.redhat.com/browse/COST-2201

--- src/api/accountSettings.ts
+++ src/api/accountSettings.ts
@@ -1,29 +1,25 @@
 import axios from 'axios';
 
-import { PagedResponse } from './api';
+import { PagedLinks, PagedMetaData } from './api';
 
-// To do: Adjust when account-settings API is available
 export interface AccountSettingsData {
-  code?: string;
-  description?: string;
-  name?: string;
-}
-
-export interface AccountSettingsMeta {
-  count?: string;
-  ['cost-type']?: string;
+  cost_type?: string;
   currency?: string;
 }
 
-export interface AccountSettings extends PagedResponse<AccountSettingsData, AccountSettingsMeta> {}
+export interface AccountSettings {
+  meta: PagedMetaData;
+  links?: PagedLinks;
+  data: AccountSettingsData;
+}
 
 export function fetchAccountSettings() {
   const insights = (window as any).insights;
   if (insights && insights.chrome && insights.chrome.auth && insights.chrome.auth.getUser) {
     return insights.chrome.auth.getUser().then(() => {
-      return axios.get<AccountSettings>(`cost-type/`); // To do: Use account-settings API when available
+      return axios.get<AccountSettings>(`account-settings/`);
     });
   } else {
-    return axios.get<AccountSettings>(`cost-type/`); // To do: Use account-settings API when available
+    return axios.get<AccountSettings>(`account-settings/`);
   }
 }
