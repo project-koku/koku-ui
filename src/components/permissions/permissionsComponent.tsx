913f6cb122a9066c3e61590844c44c5cf7b1e630
Dan Labrecque <dlabrecq@redhat.com>
2022-01-27 19:38:57 -0500

    Replaced accountSettings with PermissionsWrapper
    https://issues.redhat.com/browse/COST-2268

--- src/components/permissions/permissionsComponent.tsx
+++ src/components/permissions/permissionsComponent.tsx
@@ -1,11 +1,9 @@
-import { AccountSettings } from 'components/accountSettings';
-import { AppSettings } from 'components/appSettings';
 import { asyncComponent } from 'components/async';
 import React from 'react';
-import { getRoutePath, paths } from 'routes';
 
-const InactiveSources = asyncComponent(() => import(/* webpackChunkName: "notFound" */ 'components/inactiveSources'));
-const Permissions = asyncComponent(() => import(/* webpackChunkName: "notFound" */ 'components/permissions'));
+const PermissionsWrapper = asyncComponent(
+  () => import(/* webpackChunkName: "permissionsWrapper" */ 'components/permissions/permissionsWrapper')
+);
 
 interface State {
   // TBD...
@@ -15,20 +13,10 @@ interface State {
 export function permissionsComponent<Props>(AysncComponent) {
   class PermissionsComponent extends React.Component<Props, State> {
     public render() {
-      const path = getRoutePath((this.props as any).location);
       return (
-        <AppSettings isResetState>
-          <Permissions>
-            <InactiveSources {...this.props} />
-            {path === paths.costModels ? (
-              <AysncComponent {...this.props} />
-            ) : (
-              <AccountSettings>
-                <AysncComponent {...this.props} />
-              </AccountSettings>
-            )}
-          </Permissions>
-        </AppSettings>
+        <PermissionsWrapper>
+          <AysncComponent {...this.props} />
+        </PermissionsWrapper>
       );
     }
   }
