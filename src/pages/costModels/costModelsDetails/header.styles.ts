cfb5bd5ca7403a6961d5921883164bfc3ff81208
Dan Labrecque <dlabrecq@redhat.com>
2022-02-03 13:31:12 -0500

    Added export link to Cost model page headers
    https://issues.redhat.com/browse/COST-2305

---
+++ src/pages/costModels/costModelsDetails/header.styles.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const styles = {
+  headerContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  headerContentRight: {
+    display: 'flex',
+  },
+} as { [className: string]: React.CSSProperties };
