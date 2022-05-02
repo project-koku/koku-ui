57d455a13a1232ad444f5969e650e2daec971415
gitdallas <dallas.nicol@gmail.com>
2022-05-02 15:19:26 -0500

    remove enzyme from trendchart tests

--- src/pages/views/components/charts/trendChart/trendChart.test.tsx
+++ src/pages/views/components/charts/trendChart/trendChart.test.tsx
@@ -1,15 +1,12 @@
-jest.mock('date-fns/format');
-
-import { Chart, ChartArea } from '@patternfly/react-charts';
 import { AwsReport, AwsReportData } from 'api/reports/awsReports';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import * as utils from 'pages/views/components/charts/common/chartDatumUtils';
 import React from 'react';
 
 import { TrendChart, TrendChartProps } from './trendChart';
 
-const currentMonthReport: AwsReport = createReport('1-15-18');
-const previousMonthReport: AwsReport = createReport('12-15-17');
+const currentMonthReport: AwsReport = createReport('2018-01-15');
+const previousMonthReport: AwsReport = createReport('2017-12-15');
 
 const currentData = utils.transformReport(currentMonthReport, utils.ChartType.daily);
 const previousData = utils.transformReport(previousMonthReport, utils.ChartType.daily);
@@ -17,6 +14,7 @@ const previousData = utils.transformReport(previousMonthReport, utils.ChartType.
 const props: TrendChartProps = {
   title: 'Trend Title',
   height: 100,
+  intl: null,
   currentData,
   previousData,
   formatter: jest.fn(),
@@ -24,60 +22,19 @@ const props: TrendChartProps = {
 };
 
 test('reports are formatted to datums', () => {
-  const view = shallow(<TrendChart {...props} />);
-  const charts = view.find(ChartArea);
-  expect(charts.length).toBe(2);
-  expect(charts.at(0).prop('data')).toMatchSnapshot('previous month data');
-  expect(charts.at(1).prop('data')).toMatchSnapshot('current month data');
+  render(<TrendChart {...props} />);
+  expect(screen.getByText(props.title).parentElement).toMatchSnapshot();
+  expect(screen.queryByText(/no data/i)).toBeNull();
 });
 
 test('null previous and current reports are handled', () => {
-  const view = shallow(<TrendChart {...props} currentData={null} previousData={null} />);
-  const charts = view.find(ChartArea);
-  expect(charts.length).toBe(2);
+  render(<TrendChart {...props} currentData={null} previousData={null} />);
+  expect(screen.getAllByText(/no data/i).length).toBe(2);
 });
 
 test('height from props is used', () => {
-  const view = shallow(<TrendChart {...props} />);
-  expect(view.find(Chart).prop('height')).toBe(props.height);
-});
-
-test('labels formats with datum and value formatted from props', () => {
-  const tooltipFormatMock = jest.spyOn(utils, 'getTooltipContent');
-  const formatLabel = jest.fn();
-  tooltipFormatMock.mockImplementation(() => formatLabel);
-
-  const view = shallow(<TrendChart {...props} />);
-  const datum: utils.ChartDatum = {
-    x: 1,
-    y: 1,
-    key: '1-1-1',
-    units: 'hrs',
-  };
-  const group = view.find(Chart);
-  group.props().containerComponent.props.labels({ datum });
-  expect(formatLabel).toBeCalledWith(datum.y, datum.units, props.formatOptions);
-  expect(view.find(Chart).prop('height')).toBe(props.height);
-});
-
-test('trend is a running total', () => {
-  const multiDayReport: AwsReport = {
-    data: [createReportDataPoint('1-15-18', 1), createReportDataPoint('1-16-18', 2)],
-  };
-  const multiDaytData = utils.transformReport(multiDayReport, utils.ChartType.daily);
-  const view = shallow(<TrendChart {...props} currentData={multiDaytData} />);
-  const charts = view.find(ChartArea);
-  expect(charts.at(1).prop('data')).toMatchSnapshot('current month data');
-});
-
-test('trend is a daily value', () => {
-  const multiDayReport: AwsReport = {
-    data: [createReportDataPoint('1-15-18', 1), createReportDataPoint('1-16-18', 2)],
-  };
-  const multiDaytData = utils.transformReport(multiDayReport, utils.ChartType.daily);
-  const view = shallow(<TrendChart {...props} currentData={multiDaytData} />);
-  const charts = view.find(ChartArea);
-  expect(charts.at(1).prop('data')).toMatchSnapshot('current month data');
+  render(<TrendChart {...props} />);
+  expect(screen.getByTestId('trend-chart-wrapper').getAttribute('style')).toContain("height: 100px");
 });
 
 function createReport(date: string): AwsReport {
