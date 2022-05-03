a774c902db397fd53040b5f8287e59946c208b65
gitdallas <dallas.nicol@gmail.com>
2022-05-03 10:49:26 -0500

    remove enzyme from usageChart tests

--- src/pages/views/components/charts/usageChart/usageChart.test.tsx
+++ src/pages/views/components/charts/usageChart/usageChart.test.tsx
@@ -1,15 +1,12 @@
-jest.mock('date-fns/format');
-
-import { Chart, ChartArea } from '@patternfly/react-charts';
 import { OcpReport, OcpReportData } from 'api/reports/ocpReports';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react'
 import * as utils from 'pages/views/components/charts/common/chartDatumUtils';
 import React from 'react';
 
 import { UsageChart, UsageChartProps } from './usageChart';
 
-const currentMonthReport: OcpReport = createReport('1-15-18');
-const previousMonthReport: OcpReport = createReport('12-15-17');
+const currentMonthReport: OcpReport = createReport('2018-01-15');
+const previousMonthReport: OcpReport = createReport('2017-12-15');
 
 const currentRequestData = utils.transformReport(currentMonthReport, utils.ChartType.daily, 'date', 'request');
 const currentUsageData = utils.transformReport(currentMonthReport, utils.ChartType.daily, 'date', 'usage');
@@ -17,6 +14,7 @@ const previousRequestData = utils.transformReport(previousMonthReport, utils.Cha
 const previousUsageData = utils.transformReport(previousMonthReport, utils.ChartType.daily, 'date', 'usage');
 
 const props: UsageChartProps = {
+  intl: null,
   currentRequestData,
   currentUsageData,
   height: 100,
@@ -27,17 +25,12 @@ const props: UsageChartProps = {
 };
 
 test('reports are formatted to datums', () => {
-  const view = shallow(<UsageChart {...props} />);
-  const charts = view.find(ChartArea);
-  expect(charts.length).toBe(4);
-  expect(charts.at(0).prop('data')).toMatchSnapshot('current month usage data');
-  expect(charts.at(1).prop('data')).toMatchSnapshot('current month request data');
-  expect(charts.at(2).prop('data')).toMatchSnapshot('previous month usage data');
-  expect(charts.at(3).prop('data')).toMatchSnapshot('previous month request data');
+  render(<UsageChart {...props} />);
+  expect(screen.getByTestId("usage-chart-wrapper")).toMatchSnapshot();
 });
 
 test('null previous and current reports are handled', () => {
-  const view = shallow(
+  render(
     <UsageChart
       {...props}
       currentRequestData={null}
@@ -46,45 +39,12 @@ test('null previous and current reports are handled', () => {
       previousUsageData={null}
     />
   );
-  const charts = view.find(ChartArea);
-  expect(charts.length).toBe(4);
+  expect(screen.getAllByText(/no data/i).length).toBe(4);
 });
 
 test('height from props is used', () => {
-  const view = shallow(<UsageChart {...props} />);
-  expect(view.find(Chart).prop('height')).toBe(props.height);
-});
-
-test('labels formats with datum and value formatted from props', () => {
-  const view = shallow(<UsageChart {...props} />);
-  const datum: utils.ChartDatum = {
-    x: 1,
-    y: 1,
-    key: '1-1-1',
-    units: 'hrs',
-  };
-  const group = view.find(Chart);
-  group.props().containerComponent.props.labels({ datum });
-  expect(props.formatter).toBeCalledWith(datum.y, datum.units, props.formatOptions);
-  expect(view.find(Chart).prop('height')).toBe(props.height);
-});
-
-test('trend is a running total', () => {
-  const multiDayReport: OcpReport = {
-    data: [createReportDataPoint('1-15-18', 1), createReportDataPoint('1-16-18', 2)],
-  };
-  const view = shallow(<UsageChart {...props} currentUsageData={multiDayReport.data} />);
-  const charts = view.find(ChartArea);
-  expect(charts.at(1).prop('data')).toMatchSnapshot('current month data');
-});
-
-test('trend is a daily value', () => {
-  const multiDayReport: OcpReport = {
-    data: [createReportDataPoint('1-15-18', 1), createReportDataPoint('1-16-18', 2)],
-  };
-  const view = shallow(<UsageChart {...props} currentUsageData={multiDayReport.data} />);
-  const charts = view.find(ChartArea);
-  expect(charts.at(1).prop('data')).toMatchSnapshot('current month data');
+  render(<UsageChart {...props} />);
+  expect(screen.getByTestId('usage-chart-wrapper').getAttribute('style')).toContain("height: 100px");
 });
 
 function createReport(date: string): OcpReport {
