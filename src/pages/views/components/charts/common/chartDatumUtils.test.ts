6f4a0eb9a43482daa3511f9b099a3d22c0d00d42
gitdallas <dallas.nicol@gmail.com>
2022-05-19 14:07:08 -0500

    test chartDatumUtils tranformReport

--- src/pages/views/components/charts/common/chartDatumUtils.test.ts
+++ src/pages/views/components/charts/common/chartDatumUtils.test.ts
@@ -1,12 +1,24 @@
 import { intl } from 'components/i18n';
 import messages from 'locales/messages';
 
-import { getTooltipContent } from './chartDatumUtils';
+import { getTooltipContent, transformReport } from './chartDatumUtils';
+import { transformReportProps } from './testProps/transformReportProps';
+import { transformReportReturns } from './testProps/transformReportReturns';
 
 jest.spyOn(intl, 'formatMessage');
 
 const labelFormatFunc = getTooltipContent(jest.fn(v => v));
 
+describe('transformReport', () => {
+  test('should return transformed report', () => {
+    expect(transformReport(
+      transformReportProps.report,
+      transformReportProps.type,
+      transformReportProps.idKey,
+      transformReportProps.reportItem,
+      transformReportProps.reportItemValue)).toStrictEqual(transformReportReturns);
+  })
+});
 describe('getTooltipContent', () => {
   test('format hrs and gb', () => {
     [
