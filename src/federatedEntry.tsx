3e6fde3cc2796e7589a8a6847137458b8462bf63
gitdallas <dallas.nicol@gmail.com>
2022-06-02 09:33:30 -0500

    add properties/arguments for frontend-components-notifications

--- src/federatedEntry.tsx
+++ src/federatedEntry.tsx
@@ -32,11 +32,12 @@ const store = configureStore({
 export default () => {
   const basename = getBaseName(window.location.pathname);
   const locale = getLocale();
+  const notificationProps = {} as any;
 
   return (
     <IntlProvider defaultLocale="en" locale={locale} messages={messages[locale]} onError={console.log}>
       <Provider store={store as any}>
-        <NotificationsPortal />
+        <NotificationsPortal {...notificationProps} />
         <Router basename={basename}>
           <App basename={basename} />
         </Router>
