e3da3dcc3379b7e44dce9043e38bff963cc9ca75
Dan Labrecque <dlabrecq@redhat.com>
2022-07-25 10:44:26 -0400

    Cost model permissions should check for multiple roles
    https://issues.redhat.com/browse/COST-2816

--- src/store/rbac/selectors.ts
+++ src/store/rbac/selectors.ts
@@ -12,10 +12,21 @@ export const isCostModelWritePermission = (state: RootState) => {
   if (!permissions) {
     return false;
   }
-  const costModelPermissions = permissions.find(item => item.permission.startsWith('cost-management'));
+  const costModelPermissions = permissions.filter(item => item.permission.startsWith('cost-management'));
   if (!costModelPermissions) {
     return false;
   }
+  // Check for multiple roles; cost-management:cost-model:read and cost-management:cost-model:write
+  // See https://issues.redhat.com/browse/COST-2816
+  for (const item of costModelPermissions) {
+    if (hasWritePermission(item)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const hasWritePermission = costModelPermissions => {
   const [app, resource, operation] = costModelPermissions.permission.split(':');
   if (
     app === 'cost-management' &&
