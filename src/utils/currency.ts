ad68425571893177b92bfdf8829e423e97eee50b
Dan Labrecque <dlabrecq@redhat.com>
2022-10-04 22:20:06 -0400

    Util to retrieve currency from query or local storage
    https://issues.redhat.com/browse/COST-1906

---
+++ src/utils/currency.ts
@@ -0,0 +1,8 @@
+import { parseQuery, Query } from 'api/queries/query';
+import { getCurrency as getCurrencyFromLocaleStorage } from 'utils/localStorage';
+
+// Returns cost type
+export const getCurrency = () => {
+  const query = parseQuery<Query>(location.search);
+  return query.currency ? query.currency : getCurrencyFromLocaleStorage();
+};
