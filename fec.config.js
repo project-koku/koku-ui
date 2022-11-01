311c9bf6abfda52e33d9cf7856874f20a391b8ab
Dan Labrecque <dlabrecq@redhat.com>
2022-11-01 19:32:57 -0400

    Enable build cache and HMR
    https://issues.redhat.com/browse/COST-3220

--- fec.config.js
+++ fec.config.js
@@ -1,3 +1,5 @@
+// Based on https://github.com/RedHatInsights/frontend-components/blob/master/packages/config/src/scripts/dev.webpack.config.js
+
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
@@ -36,7 +38,9 @@ module.exports = {
   proxyVerbose: true,
   stats,
   standalone: process.env.LOCAL_API_PORT ? true : false,
+  useCache: true,
   useProxy: process.env.LOCAL_API_PORT ? false : true,
+  ...(process.env.HMR && { _unstableHotReload: process.env.HMR === 'true' }),
   /**
    * Config for federated modules
    */
