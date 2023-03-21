701235ad6e3e4fe5cc479b9656f3a81908dd8106
Dan Labrecque <dlabrecq@redhat.com>
2023-03-21 00:12:41 -0400

    Convert to npm package manager

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,27 @@
 module.exports = {
-  testEnvironment: 'jsdom',
   clearMocks: true,
+  collectCoverage: false,
+  collectCoverageFrom: ['src/**/*.tsx'],
+  coverageDirectory: './coverage/',
   fakeTimers: {
     enableGlobally: true,
   },
-  transform: {
-    '^.+\\.[jt]sx?$': '<rootDir>/test/transformTS.js',
-    '^.+\\.(jpg)$': '<rootDir>/test/transformFile.js',
-  },
-  transformIgnorePatterns: ['node_modules/(?!@patternfly/react-icons/dist/esm)'],
-  setupFiles: ['./test/testEnv.ts'],
-  testRegex: '\\.test\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   moduleDirectories: ['node_modules', '<rootDir>/src'],
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': '<rootDir>/test/styleMock.js',
   },
   roots: ['<rootDir>/src'],
+  setupFiles: ['./test/testEnv.ts'],
+  testEnvironment: 'jsdom',
   testEnvironmentOptions: {
     url: 'http://localhost/',
   },
+  testRegex: '\\.test\\.(jsx?|tsx?)$',
   testTimeout: 30000,
+  transform: {
+    '^.+\\.[jt]sx?$': '<rootDir>/test/transformTS.js',
+    '^.+\\.(jpg)$': '<rootDir>/test/transformFile.js',
+  },
+  transformIgnorePatterns: ['node_modules/(?!@patternfly/react-icons/dist/esm)'],
 };
