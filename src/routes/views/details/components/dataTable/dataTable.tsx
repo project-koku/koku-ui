d4a63a7dbdd4ec6d16860717a629728c283128e3
Dan Labrecque <dlabrecq@redhat.com>
2023-03-27 09:10:35 -0400

    Restore previous workaround for sticky table column

--- src/routes/views/details/components/dataTable/dataTable.tsx
+++ src/routes/views/details/components/dataTable/dataTable.tsx
@@ -1,3 +1,5 @@
+import './dataTable.scss';
+
 import { Bullseye, EmptyState, EmptyStateBody, EmptyStateIcon, Spinner } from '@patternfly/react-core';
 import { CalculatorIcon } from '@patternfly/react-icons/dist/esm/icons/calculator-icon';
 import type { ThProps } from '@patternfly/react-table';
@@ -129,6 +131,7 @@ class DataTable extends React.Component<DataTableProps, any> {
       <>
         <TableComposable
           aria-label={intl.formatMessage(messages.dataTableAriaLabel)}
+          className="tableOverride"
           gridBreakPoint="grid-2xl"
           hasSelectableRowCaption={isRecommendations}
         >
