2f3b4e149ca790d1ecb6ce461cc84f5f5ccfac49
Dan Labrecque <dlabrecq@redhat.com>
2023-05-12 01:13:06 -0400

    Convert optimizations to use React hooks

--- src/routes/optimizations/optimizations.tsx
+++ src/routes/optimizations/optimizations.tsx
@@ -1,52 +1,50 @@
-import { Pagination, PaginationVariant } from '@patternfly/react-core';
+import { PageSection, Pagination, PaginationVariant } from '@patternfly/react-core';
+import type { Query } from 'api/queries/query';
+import { getQuery, parseQuery } from 'api/queries/query';
 import type { RosQuery } from 'api/queries/rosQuery';
-import { getQuery, parseQuery } from 'api/queries/rosQuery';
 import type { RosReport } from 'api/ros/ros';
 import { RosPathsType, RosType } from 'api/ros/ros';
 import type { AxiosError } from 'axios';
 import messages from 'locales/messages';
-import React from 'react';
-import type { WrappedComponentProps } from 'react-intl';
-import { injectIntl } from 'react-intl';
-import { connect } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useIntl } from 'react-intl';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import type { AnyAction } from 'redux';
+import type { ThunkDispatch } from 'redux-thunk';
 import { OptimizationsTable, OptimizationsToolbar } from 'routes/components/optimizations';
 import { Loading } from 'routes/components/page/loading';
 import { NoOptimizations } from 'routes/components/page/noOptimizations';
 import { NotAvailable } from 'routes/components/page/notAvailable';
+// import { styles } from 'routes/optimizations/optimizations.styles';
+import { getGroupById, getGroupByValue } from 'routes/utils/groupBy';
 import { getOrderById, getOrderByValue } from 'routes/utils/orderBy';
-import { getRouteForQuery } from 'routes/utils/query';
-import { handleOnFilterAdded, handleOnFilterRemoved, handleOnSort } from 'routes/utils/queryNavigate';
-import { createMapStateToProps, FetchStatus } from 'store/common';
+import * as queryUtils from 'routes/utils/query';
+import type { RootState } from 'store';
+import { FetchStatus } from 'store/common';
 import { rosActions, rosSelectors } from 'store/ros';
 import { uiActions } from 'store/ui';
-import type { RouterComponentProps } from 'utils/router';
-import { withRouter } from 'utils/router';
 
 import { styles } from './optimizations.styles';
 import { OptimizationsHeader } from './optimizationsHeader';
 
-export interface OptimizationsOwnProps extends RouterComponentProps, WrappedComponentProps {
+interface OptimizationsOwnProps {
   // TBD...
 }
 
-interface OptimizationsStateProps {
-  query: RosQuery;
+export interface OptimizationsStateProps {
+  closeOptimizationsDrawer: typeof uiActions.closeOptimizationsDrawer;
   report: RosReport;
   reportError: AxiosError;
   reportFetchStatus: FetchStatus;
   reportQueryString: string;
 }
 
-interface OptimizationsDispatchProps {
-  closeOptimizationsDrawer: typeof uiActions.closeOptimizationsDrawer;
-  fetchRosReport: typeof rosActions.fetchRosReport;
+export interface OptimizationsMapProps {
+  query?: RosQuery;
 }
 
-interface OptimizationsState {
-  // TBD...
-}
-
-type OptimizationsProps = OptimizationsStateProps & OptimizationsOwnProps & OptimizationsDispatchProps;
+type OptimizationsProps = OptimizationsOwnProps;
 
 const baseQuery: RosQuery = {
   limit: 10,
@@ -59,29 +57,15 @@ const baseQuery: RosQuery = {
 const reportType = RosType.ros as any;
 const reportPathsType = RosPathsType.recommendations as any;
 
-class Optimizations extends React.Component<OptimizationsProps, OptimizationsState> {
-  protected defaultState: OptimizationsState = {};
-  public state: OptimizationsState = { ...this.defaultState };
+const Optimizations: React.FC<OptimizationsProps> = () => {
+  const [query, setQuery] = useState({ ...baseQuery });
+  const intl = useIntl();
 
-  public componentDidMount() {
-    this.updateReport();
-  }
+  const { closeOptimizationsDrawer, report, reportError, reportFetchStatus, reportQueryString } = useMapToProps({
+    query,
+  });
 
-  public componentDidUpdate(prevProps: OptimizationsProps) {
-    const { report, reportError, reportQueryString, router } = this.props;
-
-    const newQuery = prevProps.reportQueryString !== reportQueryString;
-    const noReport = !report && !reportError;
-    const noLocation = !router.location.search;
-
-    if (newQuery || noReport || noLocation) {
-      this.updateReport();
-    }
-  }
-
-  private getPagination = (isDisabled = false, isBottom = false) => {
-    const { intl, report } = this.props;
-
+  const getPagination = (isDisabled = false, isBottom = false) => {
     const count = report && report.meta ? report.meta.count : 0;
     const limit = report && report.meta ? report.meta.limit : baseQuery.limit;
     const offset = report && report.meta ? report.meta.offset : baseQuery.offset;
@@ -92,8 +76,8 @@ class Optimizations extends React.Component<OptimizationsProps, OptimizationsSta
         isCompact={!isBottom}
         isDisabled={isDisabled}
         itemCount={count}
-        onPerPageSelect={(event, perPage) => this.handleOnPerPageSelect(perPage)}
-        onSetPage={(event, pageNumber) => this.handleOnSetPage(pageNumber)}
+        onPerPageSelect={(event, perPage) => handleOnPerPageSelect(perPage)}
+        onSetPage={(event, pageNumber) => handleOnSetPage(pageNumber)}
         page={page}
         perPage={limit}
         titles={{
@@ -108,14 +92,12 @@ class Optimizations extends React.Component<OptimizationsProps, OptimizationsSta
     );
   };
 
-  private getTable = () => {
-    const { query, report, reportFetchStatus, reportQueryString, router } = this.props;
-
+  const getTable = () => {
     return (
       <OptimizationsTable
         filterBy={query.filter_by}
         isLoading={reportFetchStatus === FetchStatus.inProgress}
-        onSort={(sortType, isSortAscending) => handleOnSort(query, router, sortType, isSortAscending)}
+        onSort={(sortType, isSortAscending) => handleOnSort(sortType, isSortAscending)}
         orderBy={query.order_by}
         report={report}
         reportQueryString={reportQueryString}
@@ -123,9 +105,7 @@ class Optimizations extends React.Component<OptimizationsProps, OptimizationsSta
     );
   };
 
-  private getToolbar = () => {
-    const { query, report, router } = this.props;
-
+  const getToolbar = () => {
     const itemsPerPage = report && report.meta ? report.meta.limit : 0;
     const itemsTotal = report && report.meta ? report.meta.count : 0;
     const isDisabled = itemsTotal === 0;
@@ -136,117 +116,122 @@ class Optimizations extends React.Component<OptimizationsProps, OptimizationsSta
         isProject
         itemsPerPage={itemsPerPage}
         itemsTotal={itemsTotal}
-        onFilterAdded={filter => handleOnFilterAdded(query, router, filter)}
-        onFilterRemoved={filter => handleOnFilterRemoved(query, router, filter)}
-        pagination={this.getPagination(isDisabled)}
+        onFilterAdded={filter => handleOnFilterAdded(filter)}
+        onFilterRemoved={filter => handleOnFilterRemoved(filter)}
+        pagination={getPagination(isDisabled)}
         query={query}
       />
     );
   };
 
-  private handleOnPerPageSelect = (perPage: number) => {
-    const { closeOptimizationsDrawer, query, router } = this.props;
-
-    const newQuery = {
-      ...JSON.parse(JSON.stringify(query)),
-      limit: perPage,
-    };
-    const filteredQuery = getRouteForQuery(newQuery, router.location, true);
-    router.navigate(filteredQuery, { replace: true });
+  const handleOnFilterAdded = filter => {
+    const newQuery = queryUtils.handleOnFilterAdded(query, filter);
+    setQuery(newQuery);
     closeOptimizationsDrawer();
   };
 
-  private handleOnSetPage = (pageNumber: number) => {
-    const { closeOptimizationsDrawer, query, report, router } = this.props;
-
-    const limit = report && report.meta && report.meta.limit ? report.meta.limit : baseQuery.limit;
-    const offset = pageNumber * limit - limit;
-
-    const newQuery = {
-      ...JSON.parse(JSON.stringify(query)),
-      limit,
-      offset,
-    };
-    const filteredQuery = getRouteForQuery(newQuery, router.location);
-    router.navigate(filteredQuery, { replace: true });
+  const handleOnFilterRemoved = filter => {
+    const newQuery = queryUtils.handleOnFilterRemoved(query, filter);
+    setQuery(newQuery);
     closeOptimizationsDrawer();
   };
 
-  private updateReport = () => {
-    const { fetchRosReport, reportQueryString } = this.props;
-    fetchRosReport(reportPathsType, reportType, reportQueryString);
+  const handleOnPerPageSelect = perPage => {
+    const newQuery = queryUtils.handleOnPerPageSelect(query, perPage, true);
+    setQuery(newQuery);
+    closeOptimizationsDrawer();
   };
 
-  public render() {
-    const { intl, query, report, reportError, reportFetchStatus } = this.props;
+  const handleOnSetPage = pageNumber => {
+    const newQuery = queryUtils.handleOnSetPage(query, report, pageNumber, true);
+    setQuery(newQuery);
+    closeOptimizationsDrawer();
+  };
 
-    const itemsTotal = report && report.meta ? report.meta.count : 0;
-    const isDisabled = itemsTotal === 0;
-    const title = intl.formatMessage(messages.optimizations);
-    const hasOptimizations = report && report.meta && report.meta.count > 0;
+  const handleOnSort = (sortType, isSortAscending) => {
+    const newQuery = queryUtils.handleOnSort(query, sortType, isSortAscending);
+    setQuery(newQuery);
+    closeOptimizationsDrawer();
+  };
 
-    if (reportError) {
-      return <NotAvailable title={title} />;
-    }
-    if (!query.filter_by && !hasOptimizations && reportFetchStatus === FetchStatus.complete) {
-      return <NoOptimizations title={title} />;
-    }
-    return (
-      <div style={styles.optimizationsContainer}>
-        <OptimizationsHeader />
-        <div style={styles.content}>
-          <div style={styles.toolbarContainer}>{this.getToolbar()}</div>
-          {reportFetchStatus === FetchStatus.inProgress ? (
-            <Loading />
-          ) : (
-            <>
-              <div style={styles.tableContainer}>{this.getTable()}</div>
-              <div style={styles.paginationContainer}>
-                <div style={styles.pagination}>{this.getPagination(isDisabled, true)}</div>
-              </div>
-            </>
-          )}
-        </div>
-      </div>
-    );
+  const itemsTotal = report && report.meta ? report.meta.count : 0;
+  const isDisabled = itemsTotal === 0;
+  const title = intl.formatMessage(messages.optimizations);
+  const hasOptimizations = report && report.meta && report.meta.count > 0;
+
+  if (reportError) {
+    return <NotAvailable title={title} />;
   }
-}
+  if (!query.filter_by && !hasOptimizations && reportFetchStatus === FetchStatus.complete) {
+    return <NoOptimizations />;
+  }
+  return (
+    <div style={styles.container}>
+      <OptimizationsHeader />
+      <PageSection>
+        {getToolbar()}
+        {reportFetchStatus === FetchStatus.inProgress ? (
+          <Loading />
+        ) : (
+          <>
+            {getTable()}
+            {getPagination(isDisabled, true)}
+          </>
+        )}
+      </PageSection>
+    </div>
+  );
+};
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const mapStateToProps = createMapStateToProps<OptimizationsOwnProps, OptimizationsStateProps>((state, { router }) => {
-  const queryFromRoute = parseQuery<RosQuery>(router.location.search);
-  const order_by = getOrderById(queryFromRoute) || getOrderById(baseQuery);
-  const order_how = getOrderByValue(queryFromRoute) || getOrderByValue(baseQuery);
+const useQueryFromRoute = () => {
+  const location = useLocation();
+  return parseQuery<Query>(location.search);
+};
 
-  const query: any = {
-    ...baseQuery,
-    ...queryFromRoute,
-  };
+// eslint-disable-next-line no-empty-pattern
+const useMapToProps = ({ query }: OptimizationsMapProps): OptimizationsStateProps => {
+  const dispatch: ThunkDispatch<RootState, any, AnyAction> = useDispatch();
+  const queryFromRoute = useQueryFromRoute();
+
+  const groupBy = getGroupById(queryFromRoute);
+  const groupByValue = getGroupByValue(queryFromRoute);
+  const order_by = getOrderById(query) || getOrderById(baseQuery);
+  const order_how = getOrderByValue(query) || getOrderByValue(baseQuery);
+
   const reportQuery = {
+    ...(groupBy && {
+      [groupBy]: groupByValue, // Flattened project filter
+    }),
     ...query.filter_by, // Flattened filter by
     limit: query.limit,
     offset: query.offset,
     order_by, // Flattened order by
     order_how, // Flattened order how
   };
-
   const reportQueryString = getQuery(reportQuery);
-  const report = rosSelectors.selectRos(state, reportPathsType, reportType, reportQueryString);
-  const reportError = rosSelectors.selectRosError(state, reportPathsType, reportType, reportQueryString);
-  const reportFetchStatus = rosSelectors.selectRosFetchStatus(state, reportPathsType, reportType, reportQueryString);
+  const report = useSelector((state: RootState) =>
+    rosSelectors.selectRos(state, reportPathsType, reportType, reportQueryString)
+  );
+  const reportFetchStatus = useSelector((state: RootState) =>
+    rosSelectors.selectRosFetchStatus(state, reportPathsType, reportType, reportQueryString)
+  );
+  const reportError = useSelector((state: RootState) =>
+    rosSelectors.selectRosError(state, reportPathsType, reportType, reportQueryString)
+  );
+
+  useEffect(() => {
+    if (!reportError && reportFetchStatus !== FetchStatus.inProgress) {
+      dispatch(rosActions.fetchRosReport(reportPathsType, reportType, reportQueryString));
+    }
+  }, [query]);
 
   return {
-    query,
+    closeOptimizationsDrawer: uiActions.closeOptimizationsDrawer,
     report,
     reportError,
     reportFetchStatus,
     reportQueryString,
-  } as any;
-});
-
-const mapDispatchToProps: OptimizationsDispatchProps = {
-  closeOptimizationsDrawer: uiActions.closeOptimizationsDrawer,
-  fetchRosReport: rosActions.fetchRosReport,
+  };
 };
 
-export default injectIntl(withRouter(connect(mapStateToProps, mapDispatchToProps)(Optimizations)));
+export default Optimizations;
