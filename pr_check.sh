5a4b7571cb7718732c377f26bd16c078e5ee7d4a
Pedro Lopez <>
2023-08-03 10:15:17 +0200

    add bonfire and iqe deployment

--- pr_check.sh
+++ pr_check.sh
@@ -4,7 +4,12 @@
 # Export vars for helper scripts to use
 # --------------------------------------------
 # name of app-sre "application" folder this component lives in; needs to match for quay
-export COMPONENT="hccm"
+export APP_NAME="hccm"
+export COMPONENT_NAME="hccm-frontend"
+
+COMPONENTS="hive-metastore koku presto"
+COMPONENTS_W_RESOURCES="hive-metastore koku presto"
+
 # IMAGE should match the quay repo set by app.yaml in app-interface
 export IMAGE="quay.io/cloudservices/hccm-frontend"
 export WORKSPACE=${WORKSPACE:-$APP_ROOT} # if running in jenkins, use the build's workspace
@@ -15,22 +20,85 @@ COMMON_BUILDER=https://raw.githubusercontent.com/RedHatInsights/insights-fronten
 # --------------------------------------------
 # Options that must be configured by app owner
 # --------------------------------------------
-IQE_PLUGINS="hccm"
-IQE_MARKER_EXPRESSION="smoke"
-IQE_FILTER_EXPRESSION=""
+export IQE_PLUGINS="cost_management"
+export IQE_MARKER_EXPRESSION="cost_ui_required"
+export IQE_FILTER_EXPRESSION=""
+
+export IQE_CJI_TIMEOUT="120m"
+
+# Specific config for UI deployment and ui smokes
+export DEPLOY_FRONTENDS="true"
+export IQE_ENV="ephemeral"
+export IQE_SELENIUM="true"
+
 
 set -exv
 # source is preferred to | bash -s in this case to avoid a subshell
 source <(curl -sSL $COMMON_BUILDER/src/frontend-build.sh)
 BUILD_RESULTS=$?
 
-# Stubbed out for now
-mkdir -p $WORKSPACE/artifacts
-cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
-<testsuite tests="1">
-    <testcase classname="dummy" name="dummytest"/>
-</testsuite>
-EOF
+# bootstrap bonfire and it's config
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s "$CICD_URL"/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
-# teardown_docker
-exit $BUILD_RESULTS
+source ${CICD_ROOT}/_common_deploy_logic.sh
+export NAMESPACE=$(bonfire namespace reserve --duration 2h15m)
+
+# Get secrets from the cluster resources
+
+oc get secret/koku-aws -o json -n ephemeral-base | jq -r '.data' > aws-creds.json
+oc get secret/koku-gcp -o json -n ephemeral-base | jq -r '.data' > gcp-creds.json
+oc get secret/koku-oci -o json -n ephemeral-base | jq -r '.data' > oci-creds.json
+
+AWS_ACCESS_KEY_ID_EPH=$(jq -r '."aws-access-key-id"' < aws-creds.json | base64 -d)
+AWS_SECRET_ACCESS_KEY_EPH=$(jq -r '."aws-secret-access-key"' < aws-creds.json | base64 -d)
+GCP_CREDENTIALS_EPH=$(jq -r '."gcp-credentials"' < gcp-creds.json)
+OCI_CREDENTIALS_EPH=$(jq -r '."oci-credentials"' < oci-creds.json)
+OCI_CLI_USER_EPH=$(jq -r '."oci-cli-user"' < oci-creds.json | base64 -d)
+OCI_CLI_FINGERPRINT_EPH=$(jq -r '."oci-cli-fingerprint"' < oci-creds.json | base64 -d)
+OCI_CLI_TENANCY_EPH=$(jq -r '."oci-cli-tenancy"' < oci-creds.json | base64 -d)
+
+
+export EXTRA_DEPLOY_ARGS="--set-parameter rbac/MIN_REPLICAS=1"
+       EXTRA_DEPLOY_ARGS+=" --set-parameter koku/AWS_ACCESS_KEY_ID_EPH=${AWS_ACCESS_KEY_ID_EPH}"
+       EXTRA_DEPLOY_ARGS+=" --set-parameter koku/AWS_SECRET_ACCESS_KEY_EPH=${AWS_SECRET_ACCESS_KEY_EPH}"
+       EXTRA_DEPLOY_ARGS+=" --set-parameter koku/GCP_CREDENTIALS_EPH=${GCP_CREDENTIALS_EPH}"
+       EXTRA_DEPLOY_ARGS+=" --set-parameter koku/OCI_CREDENTIALS_EPH=${OCI_CREDENTIALS_EPH}"
+       EXTRA_DEPLOY_ARGS+=" --set-parameter koku/OCI_CLI_USER_EPH=${OCI_CLI_USER_EPH}"
+       EXTRA_DEPLOY_ARGS+=" --set-parameter koku/OCI_CLI_FINGERPRINT_EPH=${OCI_CLI_FINGERPRINT_EPH}"
+       EXTRA_DEPLOY_ARGS+=" --set-parameter koku/OCI_CLI_TENANCY_EPH=${OCI_CLI_TENANCY_EPH}"
+       EXTRA_DEPLOY_ARGS+=" --set-parameter koku/OCI_CLI_TENANCY_EPH=${OCI_CLI_TENANCY_EPH}"
+       EXTRA_DEPLOY_ARGS+=" --set-parameter koku/OCI_CLI_TENANCY_EPH=${OCI_CLI_TENANCY_EPH}"
+       EXTRA_DEPLOY_ARGS+=" --set-parameter koku/OCI_CLI_TENANCY_EPH=${OCI_CLI_TENANCY_EPH}"
+       EXTRA_DEPLOY_ARGS+=" --set-parameter koku/WORKER_SUMMARY_MIN_REPLICAS=1"
+       EXTRA_DEPLOY_ARGS+=" --set-parameter koku/WORKER_PRIORITY_MIN_REPLICAS=1"
+       EXTRA_DEPLOY_ARGS+=" --set-parameter koku/WORKER_DOWNLOAD_MIN_REPLICAS=1"
+       EXTRA_DEPLOY_ARGS+=" --set-parameter koku/LISTENER_MIN_REPLICAS=1"
+
+
+export REF_ENV= "insights-production"
+export OPTIONAL_DEPS_METHOD= "hybrid"
+
+bonfire deploy \
+    ${APP_NAME} \
+    --source=appsre \
+    --ref-env ${REF_ENV} \
+    --set-template-ref ${COMPONENT_NAME}=${GIT_COMMIT} \
+    --set-image-tag ${IMAGE}=${IMAGE_TAG} \
+    --namespace ${NAMESPACE} \
+    --timeout ${DEPLOY_TIMEOUT} \
+    --optional-deps-method ${OPTIONAL_DEPS_METHOD} \
+    --frontends ${DEPLOY_FRONTENDS} \
+    ${COMPONENTS_ARG} \
+    ${COMPONENTS_RESOURCES_ARG} \
+    ${EXTRA_DEPLOY_ARGS}
+
+
+# We need to override component to deployt iqe pod
+export COMPONENT_NAME="koku"
+source "$CICD_ROOT"/cji_smoke_test.sh
+
+
+source "$CICD_ROOT"/post_test_results.sh
+
+
