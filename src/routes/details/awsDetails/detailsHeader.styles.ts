749266415f91482f6700ae0b50d5056b01a734ee
Dan Labrecque <dlabrecq@redhat.com>
2024-03-28 09:59:10 -0400

    Refactor Azure details header to wrap better at small sizes

--- src/routes/details/awsDetails/detailsHeader.styles.ts
+++ src/routes/details/awsDetails/detailsHeader.styles.ts
@@ -1,16 +1,10 @@
 import global_BackgroundColor_light_100 from '@patternfly/react-tokens/dist/js/global_BackgroundColor_light_100';
-import global_Color_100 from '@patternfly/react-tokens/dist/js/global_Color_100';
-import global_FontSize_sm from '@patternfly/react-tokens/dist/js/global_FontSize_sm';
 import global_spacer_lg from '@patternfly/react-tokens/dist/js/global_spacer_lg';
 import global_spacer_sm from '@patternfly/react-tokens/dist/js/global_spacer_sm';
 import global_spacer_xs from '@patternfly/react-tokens/dist/js/global_spacer_xs';
 import type React from 'react';
 
 export const styles = {
-  costLabelUnit: {
-    fontSize: global_FontSize_sm.value,
-    color: global_Color_100.var,
-  },
   costValue: {
     marginTop: global_spacer_lg.var,
     marginBottom: 0,
