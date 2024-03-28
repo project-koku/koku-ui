ac1256ed65dfcf657df88ed0bea5e0309526ec4f
Dan Labrecque <dlabrecq@redhat.com>
2024-03-28 09:59:27 -0400

    Refactor OCI details header to wrap better at small sizes

--- src/routes/details/ocpDetails/detailsHeader.styles.ts
+++ src/routes/details/ocpDetails/detailsHeader.styles.ts
@@ -1,7 +1,5 @@
 import global_BackgroundColor_light_100 from '@patternfly/react-tokens/dist/js/global_BackgroundColor_light_100';
-import global_Color_100 from '@patternfly/react-tokens/dist/js/global_Color_100';
 import global_FontSize_md from '@patternfly/react-tokens/dist/js/global_FontSize_md';
-import global_FontSize_sm from '@patternfly/react-tokens/dist/js/global_FontSize_sm';
 import global_spacer_lg from '@patternfly/react-tokens/dist/js/global_spacer_lg';
 import global_spacer_sm from '@patternfly/react-tokens/dist/js/global_spacer_sm';
 import global_spacer_xs from '@patternfly/react-tokens/dist/js/global_spacer_xs';
@@ -11,10 +9,6 @@ export const styles = {
   costValue: {
     marginTop: global_spacer_lg.var,
     marginBottom: 0,
-  },
-  costLabelUnit: {
-    fontSize: global_FontSize_sm.value,
-    color: global_Color_100.var,
   },
   dateTitle: {
     textAlign: 'end',
