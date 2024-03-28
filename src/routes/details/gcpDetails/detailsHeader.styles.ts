b2b88666bd01f9bab141a7e47bf431514444c696
Dan Labrecque <dlabrecq@redhat.com>
2024-03-28 10:30:15 -0400

    Refactor GCP details header to wrap better at small sizes

--- src/routes/details/gcpDetails/detailsHeader.styles.ts
+++ src/routes/details/gcpDetails/detailsHeader.styles.ts
@@ -1,18 +1,13 @@
 import global_BackgroundColor_light_100 from '@patternfly/react-tokens/dist/js/global_BackgroundColor_light_100';
-import global_Color_100 from '@patternfly/react-tokens/dist/js/global_Color_100';
-import global_FontSize_sm from '@patternfly/react-tokens/dist/js/global_FontSize_sm';
 import global_spacer_lg from '@patternfly/react-tokens/dist/js/global_spacer_lg';
 import global_spacer_sm from '@patternfly/react-tokens/dist/js/global_spacer_sm';
+import global_spacer_xs from '@patternfly/react-tokens/dist/js/global_spacer_xs';
 import type React from 'react';
 
 export const styles = {
   costValue: {
     marginTop: global_spacer_lg.var,
     marginBottom: 0,
-  },
-  costLabelUnit: {
-    fontSize: global_FontSize_sm.value,
-    color: global_Color_100.var,
   },
   dateTitle: {
     textAlign: 'end',
@@ -21,16 +16,12 @@ export const styles = {
     backgroundColor: global_BackgroundColor_light_100.var,
     padding: global_spacer_lg.var,
   },
-  headerContent: {
-    display: 'flex',
-    justifyContent: 'space-between',
+  perspective: {
+    paddingTop: global_spacer_lg.var,
+    marginTop: global_spacer_xs.var,
   },
-  headerContentLeft: {
-    display: 'flex',
-    minHeight: '90px',
-  },
-  headerContentRight: {
-    display: 'flex',
+  perspectiveContainer: {
+    alignItems: 'unset',
   },
   title: {
     paddingBottom: global_spacer_sm.var,
