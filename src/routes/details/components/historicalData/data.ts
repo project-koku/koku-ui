b546411918460a6349bb48b4c7016b121fe88db8
Dan Labrecque <dlabrecq@redhat.com>
2024-05-03 10:15:56 -0400

    Update network data

--- src/routes/details/components/historicalData/data.ts
+++ src/routes/details/components/historicalData/data.ts
@@ -4,31 +4,91 @@
 
 export const currentData = {
   meta: {
-    // usual count limit offset currency filter group by order by exclude
+    count: 2,
+    limit: 100,
+    offset: 0,
     total: {
       usage: {
         value: 3.6801746441,
         units: 'GB',
       },
       data_transfer_in: {
-        value: 2.789192834,
+        value: 12.82510775,
         units: 'GB',
       },
       data_transfer_out: {
-        value: 0.8909818101,
+        value: 8.080656137,
         units: 'GB',
       },
+      infrastructure: {
+        raw: {
+          value: 1945.51192915,
+          units: 'USD',
+        },
+        markup: {
+          value: 0.0,
+          units: 'USD',
+        },
+        usage: {
+          value: 0.0,
+          units: 'USD',
+        },
+        total: {
+          value: 1945.51192915,
+          units: 'USD',
+        },
+      },
+      supplementary: {
+        raw: {
+          value: 0.0,
+          units: 'USD',
+        },
+        markup: {
+          value: 0.0,
+          units: 'USD',
+        },
+        usage: {
+          value: 0.0,
+          units: 'USD',
+        },
+        total: {
+          value: 0.0,
+          units: 'USD',
+        },
+      },
+      cost: {
+        raw: {
+          value: 4863.77982288,
+          units: 'USD',
+        },
+        markup: {
+          value: 0.0,
+          units: 'USD',
+        },
+        usage: {
+          value: 0.0,
+          units: 'USD',
+        },
+        total: {
+          value: 4863.77982288,
+          units: 'USD',
+        },
+      },
     },
-  } as any,
+  },
   links: {
-    // usual links info
-  } as any,
+    first:
+      '/api/cost-management/v1/reports/openshift/network/?filter%5Bresolution%5D=daily&filter%5Btime_scope_units%5D=month&filter%5Btime_scope_value%5D=-1&group_by%5Bcluster%5D=a94ea9bc-9e4f-4b91-89c2-c7099ec08427&limit=100&offset=0',
+    next: null,
+    previous: null,
+    last: '/api/cost-management/v1/reports/openshift/network/?filter%5Bresolution%5D=daily&filter%5Btime_scope_units%5D=month&filter%5Btime_scope_value%5D=-1&group_by%5Bcluster%5D=a94ea9bc-9e4f-4b91-89c2-c7099ec08427&limit=100&offset=0',
+  },
   data: [
     {
-      date: '2024-04-22',
+      date: '2024-05-01',
       values: [
         {
-          date: '2024-04-22',
+          date: '2024-05-01',
           data_transfer_in: {
             value: 3.6801746441,
             units: 'GB',
@@ -41,46 +101,134 @@ export const currentData = {
           clusters: ['Test OCP on AWS'],
           source_uuid: '6795d5e6-951c-4382-a8d3-390515d7ec3d',
           region: 'us-east-1',
+          infrastructure: {
+            raw: {
+              value: 1945.51192915,
+              units: 'USD',
+            },
+            markup: {
+              value: 0.0,
+              units: 'USD',
+            },
+            usage: {
+              value: 0.0,
+              units: 'USD',
+            },
+            total: {
+              value: 1945.5119291598,
+              units: 'USD',
+            },
+          },
+          supplementary: {
+            raw: {
+              value: 0.0,
+              units: 'USD',
+            },
+            markup: {
+              value: 0.0,
+              units: 'USD',
+            },
+            usage: {
+              value: 0.0,
+              units: 'USD',
+            },
+            total: {
+              value: 0.0,
+              units: 'USD',
+            },
+          },
+          cost: {
+            raw: {
+              value: 1945.51192915,
+              units: 'USD',
+            },
+            markup: {
+              value: 0.0,
+              units: 'USD',
+            },
+            usage: {
+              value: 0.0,
+              units: 'USD',
+            },
+            total: {
+              value: 1945.51192915,
+              units: 'USD',
+            },
+          },
         },
       ],
     },
     {
-      date: '2024-04-23',
+      date: '2024-05-02',
       values: [
         {
-          date: '2024-04-23',
+          date: '2024-05-02',
           data_transfer_in: {
-            value: 13.6801746441,
+            value: 9.1449331092,
             units: 'GB',
           },
           data_transfer_out: {
-            value: 12.6866746441,
+            value: 5.3939814929,
             units: 'GB',
           },
           resource_id: 'i-727f8dc9c567f1552',
           clusters: ['Test OCP on AWS'],
           source_uuid: '6795d5e6-951c-4382-a8d3-390515d7ec3d',
           region: 'us-east-1',
-        },
-      ],
-    },
-    {
-      date: '2024-04-24',
-      values: [
-        {
-          date: '2024-04-24',
-          data_transfer_in: {
-            value: 8.6801746441,
-            units: 'GB',
+          infrastructure: {
+            raw: {
+              value: 2918.26789372,
+              units: 'USD',
+            },
+            markup: {
+              value: 0.0,
+              units: 'USD',
+            },
+            usage: {
+              value: 0.0,
+              units: 'USD',
+            },
+            total: {
+              value: 2918.26789372,
+              units: 'USD',
+            },
           },
-          data_transfer_out: {
-            value: 7.6866746441,
-            units: 'GB',
+          supplementary: {
+            raw: {
+              value: 0.0,
+              units: 'USD',
+            },
+            markup: {
+              value: 0.0,
+              units: 'USD',
+            },
+            usage: {
+              value: 0.0,
+              units: 'USD',
+            },
+            total: {
+              value: 0.0,
+              units: 'USD',
+            },
           },
-          resource_id: 'i-727f8dc9c567f1552',
-          clusters: ['Test OCP on AWS'],
-          source_uuid: '6795d5e6-951c-4382-a8d3-390515d7ec3d',
-          region: 'us-east-1',
+          cost: {
+            raw: {
+              value: 2918.267893725,
+              units: 'USD',
+            },
+            markup: {
+              value: 0.0,
+              units: 'USD',
+            },
+            usage: {
+              value: 0.0,
+              units: 'USD',
+            },
+            total: {
+              value: 2918.267893725,
+              units: 'USD',
+            },
+          },
         },
       ],
     },
@@ -89,83 +237,231 @@ export const currentData = {
 
 export const previousData = {
   meta: {
-    // usual count limit offset currency filter group by order by exclude
+    count: 2,
+    limit: 100,
+    offset: 0,
     total: {
       usage: {
         value: 3.6801746441,
         units: 'GB',
       },
       data_transfer_in: {
-        value: 2.789192834,
+        value: 12.82510775,
         units: 'GB',
       },
       data_transfer_out: {
-        value: 0.8909818101,
+        value: 8.080656137,
         units: 'GB',
       },
+      infrastructure: {
+        raw: {
+          value: 1945.51192915,
+          units: 'USD',
+        },
+        markup: {
+          value: 0.0,
+          units: 'USD',
+        },
+        usage: {
+          value: 0.0,
+          units: 'USD',
+        },
+        total: {
+          value: 1945.51192915,
+          units: 'USD',
+        },
+      },
+      supplementary: {
+        raw: {
+          value: 0.0,
+          units: 'USD',
+        },
+        markup: {
+          value: 0.0,
+          units: 'USD',
+        },
+        usage: {
+          value: 0.0,
+          units: 'USD',
+        },
+        total: {
+          value: 0.0,
+          units: 'USD',
+        },
+      },
+      cost: {
+        raw: {
+          value: 4863.77982288,
+          units: 'USD',
+        },
+        markup: {
+          value: 0.0,
+          units: 'USD',
+        },
+        usage: {
+          value: 0.0,
+          units: 'USD',
+        },
+        total: {
+          value: 4863.77982288,
+          units: 'USD',
+        },
+      },
     },
-  } as any,
+  },
   links: {
-    // usual links info
-  } as any,
+    first:
+      '/api/cost-management/v1/reports/openshift/network/?filter%5Bresolution%5D=daily&filter%5Btime_scope_units%5D=month&filter%5Btime_scope_value%5D=-2&group_by%5Bcluster%5D=a94ea9bc-9e4f-4b91-89c2-c7099ec08427&limit=100&offset=0',
+    next: null,
+    previous: null,
+    last: '/api/cost-management/v1/reports/openshift/network/?filter%5Bresolution%5D=daily&filter%5Btime_scope_units%5D=month&filter%5Btime_scope_value%5D=-2&group_by%5Bcluster%5D=a94ea9bc-9e4f-4b91-89c2-c7099ec08427&limit=100&offset=0',
+  },
   data: [
     {
-      date: '2024-04-22',
+      date: '2024-04-01',
       values: [
         {
-          date: '2024-04-22',
+          date: '2024-04-01',
           data_transfer_in: {
-            value: 2.6801746441,
+            value: 3.6801746441,
             units: 'GB',
           },
           data_transfer_out: {
-            value: 1.6866746441,
+            value: 2.6866746441,
             units: 'GB',
           },
           resource_id: 'i-727f8dc9c567f1552',
           clusters: ['Test OCP on AWS'],
           source_uuid: '6795d5e6-951c-4382-a8d3-390515d7ec3d',
           region: 'us-east-1',
+          infrastructure: {
+            raw: {
+              value: 1945.51192915,
+              units: 'USD',
+            },
+            markup: {
+              value: 0.0,
+              units: 'USD',
+            },
+            usage: {
+              value: 0.0,
+              units: 'USD',
+            },
+            total: {
+              value: 1945.5119291598,
+              units: 'USD',
+            },
+          },
+          supplementary: {
+            raw: {
+              value: 0.0,
+              units: 'USD',
+            },
+            markup: {
+              value: 0.0,
+              units: 'USD',
+            },
+            usage: {
+              value: 0.0,
+              units: 'USD',
+            },
+            total: {
+              value: 0.0,
+              units: 'USD',
+            },
+          },
+          cost: {
+            raw: {
+              value: 1945.51192915,
+              units: 'USD',
+            },
+            markup: {
+              value: 0.0,
+              units: 'USD',
+            },
+            usage: {
+              value: 0.0,
+              units: 'USD',
+            },
+            total: {
+              value: 1945.51192915,
+              units: 'USD',
+            },
+          },
         },
       ],
     },
     {
-      date: '2024-04-23',
+      date: '2024-04-02',
       values: [
         {
-          date: '2024-04-23',
+          date: '2024-04-02',
           data_transfer_in: {
-            value: 12.6801746441,
+            value: 9.1449331092,
             units: 'GB',
           },
           data_transfer_out: {
-            value: 10.6866746441,
+            value: 5.3939814929,
             units: 'GB',
           },
           resource_id: 'i-727f8dc9c567f1552',
           clusters: ['Test OCP on AWS'],
           source_uuid: '6795d5e6-951c-4382-a8d3-390515d7ec3d',
           region: 'us-east-1',
-        },
-      ],
-    },
-    {
-      date: '2024-04-24',
-      values: [
-        {
-          date: '2024-04-24',
-          data_transfer_in: {
-            value: 10.6801746441,
-            units: 'GB',
+          infrastructure: {
+            raw: {
+              value: 2918.26789372,
+              units: 'USD',
+            },
+            markup: {
+              value: 0.0,
+              units: 'USD',
+            },
+            usage: {
+              value: 0.0,
+              units: 'USD',
+            },
+            total: {
+              value: 2918.26789372,
+              units: 'USD',
+            },
           },
-          data_transfer_out: {
-            value: 9.6866746441,
-            units: 'GB',
+          supplementary: {
+            raw: {
+              value: 0.0,
+              units: 'USD',
+            },
+            markup: {
+              value: 0.0,
+              units: 'USD',
+            },
+            usage: {
+              value: 0.0,
+              units: 'USD',
+            },
+            total: {
+              value: 0.0,
+              units: 'USD',
+            },
           },
-          resource_id: 'i-727f8dc9c567f1552',
-          clusters: ['Test OCP on AWS'],
-          source_uuid: '6795d5e6-951c-4382-a8d3-390515d7ec3d',
-          region: 'us-east-1',
+          cost: {
+            raw: {
+              value: 2918.267893725,
+              units: 'USD',
+            },
+            markup: {
+              value: 0.0,
+              units: 'USD',
+            },
+            usage: {
+              value: 0.0,
+              units: 'USD',
+            },
+            total: {
+              value: 2918.267893725,
+              units: 'USD',
+            },
+          },
         },
       ],
     },
