4677f360d33c1837f9674a3dad0a57919cc161cd
Dan Labrecque <dlabrecq@redhat.com>
2024-08-26 09:27:01 -0400

    Refactor type-ahead to test if search string matches alias
    https://issues.redhat.com/browse/COST-4855

--- src/routes/components/resourceTypeahead/resourceFetch.tsx
+++ src/routes/components/resourceTypeahead/resourceFetch.tsx
@@ -63,7 +63,7 @@ const ResourceFetch: React.FC<ResourceFetchProps> = ({
     if (resource && resource.data && resource.data.length > 0 && resourceFetchStatus !== FetchStatus.inProgress) {
       options = resource.data.map(item => {
         // The resource API typically returns just a value, but account_alias, cluster_alias, and instance_name may be preferred keys.
-        const value = !isNaN(search as any) ? item.value : item[resourceKey] || item.value;
+        const value = item[resourceKey]?.includes(search) ? item[resourceKey] : item.value;
         return {
           key: value,
           name: value,
