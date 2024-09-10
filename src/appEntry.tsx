2d1dc5e59ea51cdbd650c89be5a0b231415bb011
Dan Labrecque <dlabrecq@redhat.com>
2024-09-10 13:05:57 -0400

    Replace require with import

--- src/appEntry.tsx
+++ src/appEntry.tsx
@@ -1,4 +1,9 @@
 /* eslint-disable no-console */
+// Todo: Uncomment for use with non-shared PatternFly packages
+// import '@patternfly/patternfly/patternfly.css';
+import '@patternfly/patternfly/patternfly-addons.css';
+import './styles/global.css';
+
 import NotificationsPortal from '@redhat-cloud-services/frontend-components-notifications/NotificationPortal';
 import IntlProvider from '@redhat-cloud-services/frontend-components-translations/Provider';
 import { getLocale } from 'components/i18n';
@@ -9,12 +14,6 @@ import { Provider } from 'react-redux';
 import messages from '../locales/data.json';
 import App from './app';
 import { configureStore } from './store';
-
-// Todo: Uncomment for use with non-shared PatternFly packages
-// require.resolve('@patternfly/patternfly/patternfly.css');
-require.resolve('@patternfly/patternfly/patternfly-addons.css');
-
-import './styles/global.css';
 
 const costStore = configureStore({
   // session: {
