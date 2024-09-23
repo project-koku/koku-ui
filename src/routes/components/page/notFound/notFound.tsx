090a791246a32dd181c191db99c3860f1baef5df
Dan Labrecque <dlabrecq@redhat.com>
2024-09-23 10:51:20 -0400

    PatternFly v6 milestone

--- src/routes/components/page/notFound/notFound.tsx
+++ src/routes/components/page/notFound/notFound.tsx
@@ -1,11 +1,11 @@
+import { MissingPage } from '@patternfly/react-component-groups/dist/esm/MissingPage';
 import { PageSection } from '@patternfly/react-core';
-import { InvalidObject } from '@redhat-cloud-services/frontend-components/InvalidObject';
 import React from 'react';
 
 const NotFound = () => {
   return (
-    <PageSection>
-      <InvalidObject />
+    <PageSection hasBodyWrapper={false}>
+      <MissingPage titleText={undefined} />
     </PageSection>
   );
 };
--- src/routes/details/ocpDetails/detailsHeader.tsx
+++ src/routes/details/ocpDetails/detailsHeader.tsx
@@ -132,7 +132,7 @@ class DetailsHeaderBase extends React.Component<DetailsHeaderProps, DetailsHeade
     const { cost, infrastructureCost, supplementaryCost } = getTotalCost(report, costDistribution);
 
     return (
-      <header style={styles.header}>
+      <header>
         <Flex justifyContent={{ default: 'justifyContentSpaceBetween' }}>
           <FlexItem>
             <Title headingLevel="h1" style={styles.title} size={TitleSizes['2xl']}>
