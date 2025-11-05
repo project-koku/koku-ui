98784cd03bd998b65e3d045266db85ba7e109058
Dan Labrecque <dlabrecq@redhat.com>
2025-11-05 10:49:49 -0500

    Move scripts to common directory

---
+++ scripts/syncTranslations.js
@@ -0,0 +1,55 @@
+// Note the en.json file must be first converted to an abstract syntax tree (AST)
+
+const program = require('commander');
+const { readFileSync, writeFileSync } = require('fs');
+
+let LANG_DIR = 'locales/';
+let LOCALE = 'fr';
+
+program.option('-l, --lang-dir <dir>', 'folder with languages').option('-L, --locale <value>', 'language to generate');
+
+const rootFolder = `${process.cwd()}/`;
+
+program.parse(process.argv);
+const options = program.opts();
+if (options.langDir) {
+  LANG_DIR = options.langDir;
+}
+
+if (options.locale) {
+  LOCALE = options.locale;
+}
+
+const getTranslations = () => {
+  const filename = `${rootFolder}${LANG_DIR}en.json`;
+  return readFileSync(filename, 'utf8');
+}
+
+const getPrefixedTranslations = () => {
+  const translations = JSON.parse(getTranslations());
+
+  for (const key of Object.keys(translations)) {
+    const obj = translations[key];
+    injectLocale(obj);
+  }
+  return JSON.stringify({ ...translations }, null, 2);
+}
+
+const injectLocale = obj => {
+  if (Array.isArray(obj)) {
+    for (const item of obj) {
+      if (item.type === 0) {
+        item.value = `${LOCALE.toUpperCase()} ${item.value}`;
+        return;
+      } else {
+        injectLocale(item);
+      }
+    }
+  } else if (typeof obj === 'object') {
+    for (const key of Object.keys(obj)) {
+      injectLocale(obj[key]);
+    }
+  }
+}
+
+writeFileSync(`${rootFolder}${LANG_DIR}${LOCALE}.json`, getPrefixedTranslations());
---
+++ scripts/unused-messages.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+default()
+{
+  PATH=/usr/bin:/usr/sbin:${PATH}
+  export PATH
+
+  SCRIPT=`basename $0`
+  SCRIPT_DIR=`dirname $0`
+  SCRIPT_DIR=`cd $SCRIPT_DIR; pwd`
+
+  MSG_FILE=$SCRIPT_DIR/../src/locales/messages.ts
+
+  SRC_DIR=$SCRIPT_DIR/../src
+  TMP_DIR="/tmp/$SCRIPT.$$"
+}
+
+usage()
+{
+cat <<- EEOOFF
+
+    This script will check for potentially unused i18n messages
+
+    sh [-x] $SCRIPT [-h]
+
+    OPTIONS:
+    h       Display this message
+
+EEOOFF
+}
+
+# main()
+{
+  default
+
+  while getopts h c; do
+    case $c in
+      h) usage; exit 0;;
+      \?) usage; exit 1;;
+    esac
+  done
+
+  echo "The following MessageDescriptor IDs may be unused:"
+  echo ""
+
+  FILES=`find $SRC_DIR -type f -name \*.ts -o -name \*.tsx`
+
+  for KEY in `grep "id: '" $MSG_FILE | awk -F: '{print $2}' | sed "s|[',]||g"`
+  do
+    JUNK=`grep "messages.$KEY" $FILES`
+    if [ -z "$JUNK" ]; then
+      echo "$KEY"
+    fi
+  done
+
+  exit 0
+}
