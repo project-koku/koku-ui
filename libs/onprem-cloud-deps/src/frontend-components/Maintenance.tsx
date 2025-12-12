d3d588f54940a057e17652b0f2634f947a57dbd1
rawagner <rawagner@redhat.com>
2025-12-12 11:11:16 +0100

    Remove unnecessary Maintenance

--- libs/onprem-cloud-deps/src/frontend-components/Maintenance.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-interface MaintenanceProps {
-  description: React.ReactNode;
-  titleText: React.ReactNode;
-}
-
-export const Maintenance: React.FC<MaintenanceProps> = () => <div />;
