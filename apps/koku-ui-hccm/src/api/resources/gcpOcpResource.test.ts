79d2ddea0591a1658bdf0814f67d18e6f9be0aa0
Dan Labrecque <dlabrecq@redhat.com>
2026-01-15 10:39:24 -0500

    Fix test descriptions

--- apps/koku-ui-hccm/src/api/resources/gcpOcpResource.test.ts
+++ apps/koku-ui-hccm/src/api/resources/gcpOcpResource.test.ts
@@ -3,7 +3,7 @@ import { axiosInstance } from 'api';
 import { runResource } from './gcpOcpResource';
 import { ResourceType } from './resource';
 
-test('runExport API request for OCP on GCP', () => {
+test('runResource API request for OCP on GCP', () => {
   runResource(ResourceType.account, '');
   expect(axiosInstance.get).toHaveBeenCalledWith('resource-types/gcp-accounts/?openshift=true');
 });
