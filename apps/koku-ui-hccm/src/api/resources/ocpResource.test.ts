4abc4020c7d8bad9601a683aad436dabe90a9cb0
Dan Labrecque <dlabrecq@redhat.com>
2026-01-15 10:06:19 -0500

    Fix test import

--- apps/koku-ui-hccm/src/api/resources/ocpResource.test.ts
+++ apps/koku-ui-hccm/src/api/resources/ocpResource.test.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from 'api';
 
-import { runResource } from './ocpCloudResource';
+import { runResource } from './ocpResource';
 import { ResourceType } from './resource';
 
 test('runExport API request for OCP', () => {
