56f761787e823e36d64caadabe09f436ce1b63f9
Jonathan Kilzi <jkilzi@redhat.com>
2026-03-30 10:53:56 +0300

    feat(onprem): Upload Cypress videos on test failure
    - Updated .gitignore to exclude Cypress videos and screenshots.
    - Modified CI workflow to upload Cypress videos on test failure.
    - Enabled video recording in Cypress configuration for CI environments.
    
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- apps/koku-ui-onprem/cypress/cypress.config.ts
+++ apps/koku-ui-onprem/cypress/cypress.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents() {},
     specPattern: 'cypress/e2e/**/*.cy.ts',
     supportFile: 'cypress/support/index.ts',
     baseUrl: 'http://localhost:9000',
@@ -12,5 +11,6 @@ export default defineConfig({
     defaultCommandTimeout: 10000,
     viewportWidth: 1280,
     viewportHeight: 720,
+    video: process.env.CI ? true : false,
   },
 });
