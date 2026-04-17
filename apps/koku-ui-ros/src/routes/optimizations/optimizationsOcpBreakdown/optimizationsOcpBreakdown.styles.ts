5f0221023499213c34701a7e1bfc89b0a360ee46
Dan Labrecque <dlabrecq@redhat.com>
2026-04-17 09:31:45 -0400

    Update style sizes per code review comments

--- apps/koku-ui-ros/src/routes/optimizations/optimizationsOcpBreakdown/optimizationsOcpBreakdown.styles.ts
+++ apps/koku-ui-ros/src/routes/optimizations/optimizationsOcpBreakdown/optimizationsOcpBreakdown.styles.ts
@@ -1,14 +1,14 @@
-import t_global_spacer_2xl from '@patternfly/react-tokens/dist/js/t_global_spacer_2xl';
+import t_global_spacer_lg from '@patternfly/react-tokens/dist/js/t_global_spacer_lg';
+import t_global_spacer_md from '@patternfly/react-tokens/dist/js/t_global_spacer_md';
 import t_global_spacer_sm from '@patternfly/react-tokens/dist/js/t_global_spacer_sm';
-import t_global_spacer_xl from '@patternfly/react-tokens/dist/js/t_global_spacer_xl';
 import type React from 'react';
 
 export const styles = {
   card: {
-    marginTop: t_global_spacer_xl.var,
+    marginTop: t_global_spacer_md.var,
   },
   divider: {
-    marginTop: t_global_spacer_2xl.var,
+    marginTop: t_global_spacer_lg.var,
   },
   title: {
     marginTop: t_global_spacer_sm.var,
