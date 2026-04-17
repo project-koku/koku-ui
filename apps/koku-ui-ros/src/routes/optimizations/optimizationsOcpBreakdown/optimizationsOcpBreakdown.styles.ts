84ce29411093ccd6d2b3261dbdf975d62e33612b
Dan Labrecque <dlabrecq@redhat.com>
2026-04-17 09:00:38 -0400

    Update per code review comments

--- apps/koku-ui-ros/src/routes/optimizations/optimizationsOcpBreakdown/optimizationsOcpBreakdown.styles.ts
+++ apps/koku-ui-ros/src/routes/optimizations/optimizationsOcpBreakdown/optimizationsOcpBreakdown.styles.ts
@@ -1,6 +1,6 @@
 import t_global_spacer_2xl from '@patternfly/react-tokens/dist/js/t_global_spacer_2xl';
+import t_global_spacer_sm from '@patternfly/react-tokens/dist/js/t_global_spacer_sm';
 import t_global_spacer_xl from '@patternfly/react-tokens/dist/js/t_global_spacer_xl';
-import t_global_spacer_xs from '@patternfly/react-tokens/dist/js/t_global_spacer_xs';
 import type React from 'react';
 
 export const styles = {
@@ -11,6 +11,6 @@ export const styles = {
     marginTop: t_global_spacer_2xl.var,
   },
   title: {
-    marginTop: t_global_spacer_xs.var,
+    marginTop: t_global_spacer_sm.var,
   },
 } as { [className: string]: React.CSSProperties };
