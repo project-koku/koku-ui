c30c2abc248fc6f52e2a04e62517564b7c1ea45e
Dan Labrecque <dlabrecq@redhat.com>
2026-04-17 08:22:24 -0400

    Update styles per code review comments

--- apps/koku-ui-ros/src/routes/optimizations/optimizationsOcpBreakdown/optimizationsOcpBreakdown.styles.ts
+++ apps/koku-ui-ros/src/routes/optimizations/optimizationsOcpBreakdown/optimizationsOcpBreakdown.styles.ts
@@ -1,16 +1,16 @@
-import t_global_font_size_2xl from '@patternfly/react-tokens/dist/js/t_global_font_size_2xl';
-import t_global_font_size_xl from '@patternfly/react-tokens/dist/js/t_global_font_size_xl';
-import t_global_font_size_xs from '@patternfly/react-tokens/dist/js/t_global_font_size_xs';
+import t_global_spacer_2xl from '@patternfly/react-tokens/dist/js/t_global_spacer_2xl';
+import t_global_spacer_xl from '@patternfly/react-tokens/dist/js/t_global_spacer_xl';
+import t_global_spacer_xs from '@patternfly/react-tokens/dist/js/t_global_spacer_xs';
 import type React from 'react';
 
 export const styles = {
   card: {
-    marginTop: t_global_font_size_xl.var,
+    marginTop: t_global_spacer_xl.var,
   },
   divider: {
-    marginTop: t_global_font_size_2xl.var,
+    marginTop: t_global_spacer_2xl.var,
   },
   title: {
-    marginTop: t_global_font_size_xs.var,
+    marginTop: t_global_spacer_xs.var,
   },
 } as { [className: string]: React.CSSProperties };
