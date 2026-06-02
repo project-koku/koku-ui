9b1a951267da9383ea4137813e06ef81da460a9e
Dan Labrecque <dlabrecq@redhat.com>
2026-06-02 10:47:33 -0400

    Updated price list coed per code review comments

--- apps/koku-ui-hccm/src/locales/messages.ts
+++ apps/koku-ui-hccm/src/locales/messages.ts
@@ -1592,13 +1592,13 @@ export default defineMessages({
       'aws_category {Cost category names} ' +
       'cluster {Cluster names} ' +
       'cost_model {Cost model assigned} ' +
-      'cost_models {Assigned cost models}' +
+      'cost_models {Assigned cost models} ' +
       'cost_type {Calculation type} ' +
       'cpu {CPU} ' +
       'currency {Currency} ' +
       'description {Description} ' +
-      'end {End}' +
-      'end_date {End date}' +
+      'end {End} ' +
+      'end_date {End date} ' +
       'gcp_project {Google Cloud project names} ' +
       'group {Group} ' +
       'instance {Instance names} ' +
@@ -1625,8 +1625,8 @@ export default defineMessages({
       'status {Status} ' +
       'subscription_guid {Account names} ' +
       'source_type {Integration} ' +
-      'start {Start}' +
-      'start_date {Start date}' +
+      'start {Start} ' +
+      'start_date {Start date} ' +
       'storage {Storage} ' +
       'tag {Tag names} ' +
       'tags {Tags} ' +
